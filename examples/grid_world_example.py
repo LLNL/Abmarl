c312403c92c89342b7efa41f329ea5df5580af33
Eddie Rusu <rusu24edward@gmail.com>
2020-11-11 21:10:57 -0800

    Creating an environment with world, movement, and resources reveals some changes that need to be made

--- examples/grid_world_example.py
+++ examples/grid_world_example.py
@@ -4,6 +4,32 @@ import numpy as np
 
 from admiral.component_envs.world import WorldAgent
 from admiral.component_envs.movement import GridMovementEnv
+from admiral.component_envs.resources import GridResourceEnv
+
+class ResourceManagementEnv:
+    def __init__(self, world=None, movement=None, resource=None):
+        # TODO: require splitting world and movement
+        self.world = world
+        self.movement = movement
+        self.resource = resource
+    
+    def reset(self, **kwargs):
+        self.resource.reset(**kwargs)
+        self.movement.reset(**kwargs)
+    
+    def step(self, action_dict, **kwargs):
+        for agent_id, action in action_dict.items():
+            agent = self.world.agents[agent_id]
+            if 'move' in action:
+                agent.position = self.movement.process_move(agent.position, action['move'])
+                # TODO: Requires an update on process_move function
+            if 'harvest' in action:
+                amount_harvested = self.resource.process_harvest(agent.position, action['harvest'])
+                # Do something with the amount that is harvested.
+    
+    def render(self, **kwargs):
+        self.resource.render(**kwargs)
+        self.world.render(**kwargs)
 
 env = GridMovementEnv(
     region=10,
