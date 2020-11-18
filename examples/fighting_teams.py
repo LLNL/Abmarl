208b4b493522a3ad472625e49e30fd9d070105e2
Eddie Rusu <rusu1@llnl.gov>
2020-11-18 13:18:05 -0800

    Added a separate example for teams fighting each other without resources

--- examples/fighting_teams.py
+++ examples/fighting_teams.py
@@ -4,27 +4,24 @@ import numpy as np
 
 from admiral.component_envs.world import GridWorldEnv
 from admiral.component_envs.movement import GridMovementEnv, GridMovementAgent
-from admiral.component_envs.resources import GridResourceEnv, GridResourceAgent
-from admiral.component_envs.attacking import GridAttackingEnv, GridAttackingAgent
+from admiral.component_envs.attacking import GridAttackingTeamEnv, GridAttackingTeamAgent
 from admiral.component_envs.death_life import DyingAgent, DyingEnv
 
-class FightForResourcesAgent(DyingAgent, GridAttackingAgent, GridMovementAgent, GridResourceAgent):
+class FightingTeamsAgent(DyingAgent, GridAttackingTeamAgent, GridMovementAgent):
     pass
 
-class FightForResourcesEnv:
+class FightingTeamsEnv:
     def __init__(self, **kwargs):
         self.agents = kwargs['agents']
         self.world = GridWorldEnv(**kwargs)
-        self.resource = GridResourceEnv(**kwargs)
         self.movement = GridMovementEnv(**kwargs)
-        self.attacking = GridAttackingEnv(**kwargs)
+        self.attacking = GridAttackingTeamEnv(**kwargs)
         self.dying = DyingEnv(**kwargs)
 
         self.attacking_record = []
     
     def reset(self, **kwargs):
         self.world.reset(**kwargs)
-        self.resource.reset(**kwargs)
         self.dying.reset(**kwargs)
     
     def step(self, action_dict, **kwargs):
@@ -35,12 +32,10 @@ class FightForResourcesEnv:
                     attacked_agent = self.attacking.process_attack(agent)
                     if attacked_agent is not None:
                         self.agents[attacked_agent].health -= agent.attack_strength
+                        agent.health += agent.attack_strength # Gain health from a good attack.
                         self.attacking_record.append(agent.id + " attacked " + attacked_agent)
                 if 'move' in action:
                     agent.position = self.movement.process_move(agent.position, action['move'])
-                if 'harvest' in action:
-                    amount_harvested = self.resource.process_harvest(tuple(agent.position), action['harvest'])
-                    agent.health += amount_harvested
             
         # Because agents can affect each others' health, we process the dying
         # outside the loop at the end of all the moves. Note: this does not
@@ -48,39 +43,35 @@ class FightForResourcesEnv:
         for agent_id in action_dict:
             agent = self.agents[agent_id]
             if agent.is_alive:
-                self.dying.apply_entropy(agent)
                 self.dying.process_death(agent)
-        
-        self.resource.regrow()
     
     def render(self, fig=None, **kwargs):
         fig.clear()
-        self.resource.render(fig=fig, **kwargs)
         render_condition = {agent.id: agent.is_alive for agent in self.agents.values()}
-        self.world.render(fig=fig, render_condition=render_condition, **kwargs)
+        shape = {agent.id: 'o' if agent.team == 0 else 's' for agent in self.agents.values()}
+        self.world.render(fig=fig, render_condition=render_condition, shape_dict=shape, **kwargs)
         for record in self.attacking_record:
             print(record)
         self.attacking_record.clear()
         plt.plot()
         plt.pause(1e-6)
 
-agents = {f'agent{i}': FightForResourcesAgent(
-    id=f'agent{i}', attack_range=1, attack_strength=0.4, move=1, max_harvest=1.0
-) for i in range(6)}
-env = FightForResourcesEnv(
-    region=10,
+agents = {f'agent{i}': FightingTeamsAgent(
+    id=f'agent{i}', attack_range=1, attack_strength=0.4, team=i%2, move=1
+) for i in range(24)}
+env = FightingTeamsEnv(
+    region=12,
     agents=agents
 )
 env.reset()
 fig = plt.gcf()
 env.render(fig=fig)
 
-for _ in range(50):
+for _ in range(100):
     action_dict = {}
     for agent_id, agent in env.agents.items():
         action_dict[agent_id] = {
             'move': agent.action_space['move'].sample(),
-            'harvest': agent.action_space['harvest'].sample(),
             'attack': agent.action_space['attack'].sample(),
         }
     env.step(action_dict)
