95b51cbc63ed3195ba40d67b10972a8f0c656ba1
Eddie Rusu <rusu1@llnl.gov>
2020-11-22 15:59:59 -0800

    World module support multiple types of agents

--- admiral/component_envs/world.py
+++ admiral/component_envs/world.py
@@ -4,16 +4,16 @@ import numpy as np
 from admiral.envs import Agent
 from admiral.tools.matplotlib_utils import mscatter
 
+from admiral.component_envs.team import TeamAgent
+
 class GridWorldAgent(Agent):
     """
     WorldAgents have a position in the world. This position can given to the agent
     as a starting position for each episode. If the position is None, then the environment
     should assign the agent a random position.
     """
-    def __init__(self, view=None, starting_position=None, **kwargs):
+    def __init__(self, starting_position=None, **kwargs):
         super().__init__(**kwargs)
-        assert view is not None, "GridWorldAgent must have a view"
-        self.view = view
         self.starting_position = starting_position
     
     @property
@@ -21,6 +21,16 @@ class GridWorldAgent(Agent):
         """
         Determine if the agent has been successfully configured.
         """
+        return super().configured
+
+class GridWorldObservingAgent(GridWorldAgent):
+    def __init__(self, view=None, **kwargs):
+        assert view is not None, "GridWorldObservationAgent must have a view"
+        self.view = view
+        super().__init__(**kwargs)
+    
+    @property
+    def configured(self):
         return super().configured and self.view is not None
 
 class GridWorldEnv:
@@ -33,14 +43,14 @@ class GridWorldEnv:
         assert type(region) is int, "Region must be an integer."
         self.region = region
         assert type(agents) is dict, "agents must be a dict"
-        # for agent in agents.values():
-        #     assert isinstance(agent, GridWorldAgent)
-        # self.agents = agents
-        self.agents = {agent.id: agent for agent in agents.values() if isinstance(agent, GridWorldAgent)}
+        for agent in self.agents.values():
+            assert isinstance(agent, GridWorldAgent)
+        self.agents = agents
 
         from gym.spaces import Box
         for agent in self.agents.values():
-            agent.observation_space['agents'] = Box(-1, 1, (agent.view*2+1, agent.view*2+1), np.int)
+            if isinstance(agent, GridWorldObservingAgent):
+                agent.observation_space['agents'] = Box(-1, 1, (agent.view*2+1, agent.view*2+1), np.int)
 
     def reset(self, **kwargs):
         """
@@ -89,80 +99,76 @@ class GridWorldEnv:
         return ax
     
     def get_obs(self, my_id, **kwargs):
-        # TODO: smart check the get obs on agent_id
-        my_agent = self.agents[my_id]
-        signal = np.zeros((my_agent.view*2+1, my_agent.view*2+1))
+        if isinstance(self.agents, GridWorldObservingAgent):
+            my_agent = self.agents[my_id]
+            signal = np.zeros((my_agent.view*2+1, my_agent.view*2+1))
 
-        # --- Determine the boundaries of the agents' grids --- #
-        # For left and top, we just do: view - x,y >= 0
-        # For the right and bottom, we just do region - x,y - 1 - view > 0
-        if my_agent.view - my_agent.position[0] >= 0: # Top end
-            signal[0:my_agent.view - my_agent.position[0], :] = -1
-        if my_agent.view - my_agent.position[1] >= 0: # Left end
-            signal[:, 0:my_agent.view - my_agent.position[1]] = -1
-        if self.region - my_agent.position[0] - my_agent.view - 1 < 0: # Bottom end
-            signal[self.region - my_agent.position[0] - my_agent.view - 1:,:] = -1
-        if self.region - my_agent.position[1] - my_agent.view - 1 < 0: # Right end
-            signal[:, self.region - my_agent.position[1] - my_agent.view - 1:] = -1
+            # --- Determine the boundaries of the agents' grids --- #
+            # For left and top, we just do: view - x,y >= 0
+            # For the right and bottom, we just do region - x,y - 1 - view > 0
+            if my_agent.view - my_agent.position[0] >= 0: # Top end
+                signal[0:my_agent.view - my_agent.position[0], :] = -1
+            if my_agent.view - my_agent.position[1] >= 0: # Left end
+                signal[:, 0:my_agent.view - my_agent.position[1]] = -1
+            if self.region - my_agent.position[0] - my_agent.view - 1 < 0: # Bottom end
+                signal[self.region - my_agent.position[0] - my_agent.view - 1:,:] = -1
+            if self.region - my_agent.position[1] - my_agent.view - 1 < 0: # Right end
+                signal[:, self.region - my_agent.position[1] - my_agent.view - 1:] = -1
 
-        # --- Determine the positions of all the other alive agents --- #
-        for other_id, other_agent in self.agents.items():
-            if other_id == my_id: continue # Don't observe yourself
-            r_diff = other_agent.position[0] - my_agent.position[0]
-            c_diff = other_agent.position[1] - my_agent.position[1]
-            if -my_agent.view <= r_diff <= my_agent.view and -my_agent.view <= c_diff <= my_agent.view:
-                r_diff += my_agent.view
-                c_diff += my_agent.view
-                signal[r_diff, c_diff] = 1 # There is an agent at this location.
+            # --- Determine the positions of all the other alive agents --- #
+            for other_id, other_agent in self.agents.items():
+                if other_id == my_id: continue # Don't observe yourself
+                r_diff = other_agent.position[0] - my_agent.position[0]
+                c_diff = other_agent.position[1] - my_agent.position[1]
+                if -my_agent.view <= r_diff <= my_agent.view and -my_agent.view <= c_diff <= my_agent.view:
+                    r_diff += my_agent.view
+                    c_diff += my_agent.view
+                    signal[r_diff, c_diff] = 1 # There is an agent at this location.
 
-        return signal
+            return signal
 
-from admiral.component_envs.team import TeamAgent
-
-class GridWorldTeamAgent(TeamAgent, GridWorldAgent):
+class GridWorldObservingTeamAgent(TeamAgent, GridWorldObservingAgent):
     pass
 
 class GridWorldTeamsEnv(GridWorldEnv):
     def __init__(self, region=None, agents=None, number_of_teams=None, **kwargs):
         self.region = region
-        self.agents = {agent.id: agent for agent in agents.values() if isinstance(agent, GridWorldTeamAgent)}
+        self.agents = agents
         self.number_of_teams = number_of_teams
 
         from gym.spaces import Box
         for agent in self.agents.values():
-            agent.observation_space['agents'] = Box(-1, np.inf, (agent.view*2+1, agent.view*2+1, number_of_teams), np.int)
+            if isinstance(agent, GridWorldObservingTeamAgent):
+                agent.observation_space['agents'] = Box(-1, np.inf, (agent.view*2+1, agent.view*2+1, number_of_teams), np.int)
     
     def get_obs(self, my_id, **kwargs):
-        # TODO: smart check the get obs on agent_id
-        my_agent = self.agents[my_id]
-        signal = np.zeros((my_agent.view*2+1, my_agent.view*2+1))
+        if isinstance(self.agents, GridWorldObservingTeamAgent):
+            my_agent = self.agents[my_id]
+            signal = np.zeros((my_agent.view*2+1, my_agent.view*2+1))
 
-        # --- Determine the boundaries of the agents' grids --- #
-        # For left and top, we just do: view - x,y >= 0
-        # For the right and bottom, we just do region - x,y - 1 - view > 0
-        if my_agent.view - my_agent.position[0] >= 0: # Top end
-            signal[0:my_agent.view - my_agent.position[0], :] = -1
-        if my_agent.view - my_agent.position[1] >= 0: # Left end
-            signal[:, 0:my_agent.view - my_agent.position[1]] = -1
-        if self.region - my_agent.position[0] - my_agent.view - 1 < 0: # Bottom end
-            signal[self.region - my_agent.position[0] - my_agent.view - 1:,:] = -1
-        if self.region - my_agent.position[1] - my_agent.view - 1 < 0: # Right end
-            signal[:, self.region - my_agent.position[1] - my_agent.view - 1:] = -1
+            # --- Determine the boundaries of the agents' grids --- #
+            # For left and top, we just do: view - x,y >= 0
+            # For the right and bottom, we just do region - x,y - 1 - view > 0
+            if my_agent.view - my_agent.position[0] >= 0: # Top end
+                signal[0:my_agent.view - my_agent.position[0], :] = -1
+            if my_agent.view - my_agent.position[1] >= 0: # Left end
+                signal[:, 0:my_agent.view - my_agent.position[1]] = -1
+            if self.region - my_agent.position[0] - my_agent.view - 1 < 0: # Bottom end
+                signal[self.region - my_agent.position[0] - my_agent.view - 1:,:] = -1
+            if self.region - my_agent.position[1] - my_agent.view - 1 < 0: # Right end
+                signal[:, self.region - my_agent.position[1] - my_agent.view - 1:] = -1
 
-        # Repeat the boundaries signal for all agents
-        signal = np.repeat(signal[:, :, np.newaxis], self.number_of_teams, axis=2)
+            # Repeat the boundaries signal for all teams
+            signal = np.repeat(signal[:, :, np.newaxis], self.number_of_teams, axis=2)
 
-        # --- Determine the positions of all the other alive agents --- #
-        for other_id, other_agent in self.agents.items():
-            if other_id == my_id: continue # Don't observe yourself
-            r_diff = other_agent.position[0] - my_agent.position[0]
-            c_diff = other_agent.position[1] - my_agent.position[1]
-            if -my_agent.view <= r_diff <= my_agent.view and -my_agent.view <= c_diff <= my_agent.view:
-                r_diff += my_agent.view
-                c_diff += my_agent.view
-                signal[r_diff, c_diff, other_agent.team-1] += 1
+            # --- Determine the positions of all the other alive agents --- #
+            for other_id, other_agent in self.agents.items():
+                if other_id == my_id: continue # Don't observe yourself
+                r_diff = other_agent.position[0] - my_agent.position[0]
+                c_diff = other_agent.position[1] - my_agent.position[1]
+                if -my_agent.view <= r_diff <= my_agent.view and -my_agent.view <= c_diff <= my_agent.view:
+                    r_diff += my_agent.view
+                    c_diff += my_agent.view
+                    signal[r_diff, c_diff, other_agent.team-1] += 1
 
-        return signal
-
-
-
+            return signal
