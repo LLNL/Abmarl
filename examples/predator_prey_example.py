cef581770b77598012595840f68eeecc494ecf6c
Eddie Rusu <rusu1@llnl.gov>
2020-11-22 19:51:47 -0800

    Updated Predator Prey example with new modules

--- examples/predator_prey_example.py
+++ examples/predator_prey_example.py
@@ -2,18 +2,17 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-from admiral.component_envs.world import GridWorldTeamsEnv, GridWorldTeamAgent
+from admiral.component_envs.world import GridWorldTeamsEnv, GridWorldObservingTeamAgent
 from admiral.component_envs.movement import GridMovementEnv, GridMovementAgent
 from admiral.component_envs.attacking import GridAttackingTeamEnv, GridWorldAttackingTeamAgent
 from admiral.component_envs.death_life import DyingEnv, DyingAgent
-from admiral.component_envs.resources import GridResourceEnv, GridResourceAgent
+from admiral.component_envs.resources import GridResourceEnv, GridResourceHarvestingAndObservingAgent, GridResourceObservingAgent
 
-class PreyAgent(GridWorldTeamAgent, GridMovementAgent, DyingAgent, GridResourceAgent):
+class PreyAgent(GridWorldObservingTeamAgent, GridMovementAgent, DyingAgent, GridResourceHarvestingAndObservingAgent):
     pass
 
-class PredatorAgent(GridMovementAgent, GridWorldAttackingTeamAgent, DyingAgent):
+class PredatorAgent(GridWorldObservingTeamAgent, GridMovementAgent, GridWorldAttackingTeamAgent, DyingAgent, GridResourceObservingAgent):
     pass
-# TODO: Resolve the Method resolution bug between world and attacking agents
 
 class PredatorPreyEnv:
     def __init__(self, **kwargs):
@@ -50,7 +49,8 @@ class PredatorPreyEnv:
                         self.agents[attacked_agent].health -= agent.attack_strength
                         agent.health += agent.attack_strength # Gain health from a good attack.
                         self.attacking_record.append(agent.id + " attacked " + attacked_agent)
-                if action.get('harvest', GridWorldAttackingTeamAgent      amount_harvested = self.resource.process_harvest(tuple(agent.position), action['harvest'])
+                if action.get('harvest', False):
+                    amount_harvested = self.resource.process_harvest(tuple(agent.position), action['harvest'])
                     agent.health += amount_harvested
 
         for agent_id in action_dict:
@@ -76,22 +76,6 @@ class PredatorPreyEnv:
         plt.pause(1e-6)
     
     def get_obs(self, agent_id, **kwargs):
-        # The observation that is returned actually depends on the agent type. Predators will just
-        # see agents, so only world will return. Prey will see agents and resources, so both will
-        # return.
-        # TODO: The bigger probem there is that I want the predators to observe
-        # the resources as well, I just don't want them to have the harvest action
-        # in their action space. This means that the resources module needs to
-        # know about the predators in its self.agents in order to give them the
-        # correct observation_space and to get the correct get_obs, but it must
-        # not consider it for the processing action.
-        # Hmm.... What if resources.get_obs didn't
-        # take the agent's id but just took the values it needed to know, namely
-        # position and view? That's fine for get obs, but it doesn't solve the
-        # problem because the agent's observation_space still needs the resources.
-        if isinstance(self.agents[agent_id], PredatorAgent):
-            return {'agents': self.world.get_obs(agent_id)}
-        elif isinstance(self.agents[agent_id], PreyAgent):
             return {'agents': self.world.get_obs(agent_id), 'resources': self.resource.get_obs(agent_id)}
 
 prey = {f'prey{i}': PreyAgent(id=f'prey{i}', view=5, team=1, move=1, attack_range=-1, attack_strength=0.0, max_harvest=0.5) for i in range(7)}
@@ -109,7 +93,7 @@ print({agent_id: env.get_obs(agent_id) for agent_id in env.agents})
 fig = plt.gcf()
 env.render(fig=fig)
 
-for _ in range(30):
+for _ in range(50):
     action_dict = {agent.id: agent.action_space.sample() for agent in env.agents.values()}
     env.step(action_dict)
     env.render(fig=fig)
