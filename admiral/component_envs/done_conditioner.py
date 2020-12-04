cc4fe063c0a95bd35da88201d7b5899606dd4ebe
Eddie Rusu <rusu24edward@gmail.com>
2020-12-04 09:55:32 -0800

    Added tests for done conditions

--- admiral/component_envs/done_conditioner.py
+++ admiral/component_envs/done_conditioner.py
@@ -4,17 +4,6 @@ import numpy as np
 from admiral.component_envs.death_life import DyingAgent
 from admiral.component_envs.team import TeamAgent
 
-class DoneConditioner:
-    def __init__(self, agents=None, **kwargs):
-        self.agents = agents
-        self.dones = {agent_id: False for agent_id in self.agents}
-
-    def get_done(self, agent_id, **kwargs):
-        return self.dones[agent_id]
-    
-    def get_all_done(self, **kwargs):
-        return all([self.get_done(agent_id) for agent_id in self.agents])
-
 class DeadDoneComponent:
     def __init__(self, agents=None, **kwargs):
         self.agents = agents
---
+++ tests/test_done_component.py
@@ -0,0 +1,74 @@
+
+from admiral.component_envs.done_conditioner import DyingAgent, TeamAgent
+from admiral.component_envs.done_conditioner import DeadDoneComponent, TeamDeadDoneComponent
+
+class DoneTestAgent(DyingAgent, TeamAgent): pass
+
+def test_dead_done_condition():
+    agents = {
+        'agent0': DyingAgent(id='agent0'),
+        'agent1': DyingAgent(id='agent1'),
+        'agent2': DyingAgent(id='agent2'),
+        'agent3': DyingAgent(id='agent3'),
+    }
+    component = DeadDoneComponent(agents=agents)
+    assert component.get_done('agent0') is False
+    assert component.get_done('agent1') is False
+    assert component.get_done('agent2') is False
+    assert component.get_done('agent3') is False
+    assert component.get_all_done() is False
+
+    agents['agent0'].is_alive = False
+    agents['agent1'].is_alive = False
+    assert component.get_done('agent0')
+    assert component.get_done('agent1')
+    assert component.get_done('agent2') is False
+    assert component.get_done('agent3') is False
+    assert component.get_all_done() is False
+
+    agents['agent2'].is_alive = False
+    agents['agent3'].is_alive = False
+    assert component.get_done('agent0')
+    assert component.get_done('agent1')
+    assert component.get_done('agent2')
+    assert component.get_done('agent3')
+    assert component.get_all_done()
+
+def test_team_dead_done_condition():
+    agents = {
+        'agent0': DoneTestAgent(id='agent0', team=0),
+        'agent1': DoneTestAgent(id='agent1', team=1),
+        'agent2': DoneTestAgent(id='agent2', team=0),
+        'agent3': DoneTestAgent(id='agent3', team=1),
+        'agent4': DoneTestAgent(id='agent4', team=0),
+        'agent5': DoneTestAgent(id='agent5', team=2),
+    }
+    component = TeamDeadDoneComponent(agents=agents, number_of_teams=3)
+
+    assert component.get_done('agent0') is False
+    assert component.get_done('agent1') is False
+    assert component.get_done('agent2') is False
+    assert component.get_done('agent3') is False
+    assert component.get_done('agent4') is False
+    assert component.get_done('agent5') is False
+    assert component.get_all_done() is False
+
+    agents['agent5'].is_alive = False
+    agents['agent4'].is_alive = False
+    assert component.get_done('agent0') is False
+    assert component.get_done('agent1') is False
+    assert component.get_done('agent2') is False
+    assert component.get_done('agent3') is False
+    assert component.get_done('agent4')
+    assert component.get_done('agent5')
+    assert component.get_all_done() is False
+
+    agents['agent1'].is_alive = False
+    agents['agent3'].is_alive = False
+    assert component.get_done('agent0') is False
+    assert component.get_done('agent1')
+    assert component.get_done('agent2') is False
+    assert component.get_done('agent3')
+    assert component.get_done('agent4')
+    assert component.get_done('agent5')
+    assert component.get_all_done()
