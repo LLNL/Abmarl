f1bf4b527a812a3bf45a44f46f0a73ac0a683115
Eddie Rusu <rusu1@llnl.gov>
2020-12-07 16:23:40 -0800

    Docstrings for observer component

--- admiral/component_envs/observer.py
+++ admiral/component_envs/observer.py
@@ -2,6 +2,13 @@
 from admiral.envs import Agent
 
 class ObservingAgent(Agent):
+    """
+    Agents can observe features of the environment up to a certain view distance
+    away.
+
+    view (int):
+        The view distance.
+    """
     def __init__(self, view=None, **kwargs):
         super().__init__(**kwargs)
         assert view is not None, "view must be nonnegative integer"
@@ -9,4 +16,7 @@ class ObservingAgent(Agent):
 
     @property
     def configured(self):
+        """
+        Agents are configured if the view is set.
+        """
         return super().configured and self.view is not None
