02972603af9680b867e048c63ce7af4174272839
Eddie Rusu <rusu24edward@gmail.com>
2020-12-09 17:27:44 -0800

    Initial conversion of attacking components to new component infrastructure

--- admiral/envs/components/attacking.py
+++ admiral/envs/components/attacking.py
@@ -4,13 +4,13 @@ from admiral.envs.components.position import GridPositionAgent
 from admiral.envs.components.team import TeamAgent
 from admiral.envs.components.death_life import LifeAgent
 
-class GridAttackingAgent(Agent):
+class AttackingAgent(Agent):
     """
-    Agents that can attack other agents in the grid. Attack is based on the relative
-    positions of the agents and can affect the agents' health, if applicable.
+    Agents that can attack other agents.
 
     attack_range (int):
-        The effective range of the attack.
+        The effective range of the attack. Can be used to determine if an attack
+        is successful based on distance between agents.
     
     attack_strength (float):
         How effective the agent's attack is. This is applicable in situations where
@@ -31,101 +31,137 @@ class GridAttackingAgent(Agent):
         """
         return super().configured and self.attack_range is not None and self.attack_strength is not None
 
-class GridAttackingComponent:
+class PositionBasedHealthExchangeAttackActor:
     """
-    Provide the necessary action space for agents who can attack and process such
+    Provide the necessary action space for agents who can attack and processes such
     attacks. The attack is successful if the attacked agent is alive and within
     range. The action space is appended with a MultiBinary(1), allowing the agent
     to attack or not attack.
 
+    position_state (PositionState):
+        The attack is based on the distance between agents, so we need to query
+        the position part of the state.
+    
+    life_state (LifeState):
+        Successful attacks will change the health of the involved agents. Additionally,
+        agents can only attack other agents that are still alive.
+
     agents (dict):
-        The dictionary of agents. Because attacks are grid-based, all agents must
-        be GridPositionAgents; becuase the attacked agent must be alive, all agents
+        The dictionary of agents. Because attacks are distance-based, all agents must
+        be PositionAgents; becuase the attacked agent must be alive, all agents
         must be LifeAgents.
     """
-    def __init__(self, agents=None, **kwargs):
+    def __init__(self, position_state=None, life_state=None, agents=None, **kwargs):
+        self.position_state = position_state
+        self.life_state = life_state
         assert type(agents) is dict, "agents must be a dict"
         for agent in agents.values():
-            assert isinstance(agent, GridPositionAgent)
+            assert isinstance(agent, PositionAgent)
             assert isinstance(agent, LifeAgent)
         self.agents = agents
 
         from gym.spaces import MultiBinary
         for agent in self.agents.values():
-            if isinstance(agent, GridAttackingAgent):
+            if isinstance(agent, AttackingAgent):
                 agent.action_space['attack'] = MultiBinary(1)
 
-    def process_attack(self, attacking_agent, **kwargs):
+    # TODO: Can I do it by agent object instead of agent_id?
+    def process_attack(self, attacking_agent_id, attack, **kwargs):
         """
-        Determine which agent the attacking agent successfully attacks and return
-        that agent's id. If the attack fails, return None.
+        Determine which agent the attacking agent successfully attacks. Attacked
+        agent's health will decrease, while attacking agent's health will increase
+        by the attacking agent's attack_strength.
 
-        attacking_agent (agent):
-            The agent that has chosen to attack.
+        attacking_agent_id (str):
+            The agent that we are processing.
+
+        attack (bool):
+            True if the agent has chosen to attack, otherwise False.
         """
-        for agent in self.agents.values():
-            if agent.id == attacking_agent.id:
-                # Cannot attack yourself
-                continue
-            elif not agent.is_alive:
-                # Cannot attack dead agents
-                continue
-            elif abs(attacking_agent.position[0] - agent.position[0]) > attacking_agent.attack_range or \
-               abs(attacking_agent.position[1] - agent.position[1]) > attacking_agent.attack_range:
-                # Agent too far away
-                continue
-            else:
-                # Agent was successfully attacked
-                return agent.id
+        if attack:
+            attacking_agent = self.agents[attacking_agent_id]
+            for attacked_agent_id, attacked_agent in self.agents.items():
+                if attacked_agent_id == attacking_agent_id:
+                    # Cannot attack yourself
+                    continue
+                # TODO: This is super verbose.... Maybe I should avoid using the
+                # get state and only take the component that I need to modify?
+                # Queries can just be done on the agent objects themselves.
+                elif not self.life_state.get_state(attacked_agent_id):
+                    # Cannot attack dead agents
+                    continue
+                elif abs(self.position_state.get_state(attacked_agent_id)[0] - self.position_state.get_state(attacking_agent_id)[0]) > attacking_agent.attack_range or \
+                     abs(self.position_state.get_state(attacked_agent_id)[1] - self.position_state.get_state(attacking_agent_id)[1]) > attacking_agent.attack_range:
+                    # Agent too far away
+                    continue
+                else:
+                    # Agent was successfully attacked
+                    # TODO: HealthExchangeActor can process the actual change of
+                    # health state in the agents. So then we can use the same AttackActor
+                    # but with a different HealthExchangeActor for different effects,
+                    # such as cases where we only want the health to decrease for
+                    # the attacked agent.
+                    self.life_state.modify_health(attacked_agent_id, -attacking_agent.attack_strength, **kwargs)
+                    self.life_state.modify_health(attacking_agent_id, attacking_agent.attack_strength, **kwargs)
 
-class GridAttackingTeamComponent:
+class PositionTeamBasedHealthExchangeAttackActor:
     """
     Provide the necessary action space for agents who can attack and process such
     attacks. The attack is successful if the attacked agent is alive, on a different
     team, and within range. The action space is appended with a MultiBinary(1),
     allowing the agent to attack or not attack.
 
-    agents (dict):
-        The dictionary of agents. Because attacks are grid-based, all agents must
-        be GridPositionAgents; becuase the attacked agent must be alive, all agents
+    life_state (LifeState):
+        Successful attacks will change the health of the involved agents. Additionally,
+        agents can only attack other agents that are still alive.
+
+    agents (dict):grid
+        The dictionary of agents. Because attacks are distance-based, all agents must
+        be PositionAgents; because the attacked agent must be alive, all agents
         must be LifeAgents; and because the attacked agent must be on a different
-        team, each agent must be a TeamAgent.
+        team, all agents must be TeamAgents.
     """
-    def __init__(self, agents=None, **kwargs):
+    def __init__(self, life_state=None, agents=None, **kwargs):
+        self.life_state=life_state
         assert type(agents) is dict, "agents must be a dict"
         for agent in agents.values():
-            assert isinstance(agent, GridPositionAgent)
+            assert isinstance(agent, PositionAgent)
             assert isinstance(agent, TeamAgent)
             assert isinstance(agent, LifeAgent)
         self.agents = agents
 
         from gym.spaces import MultiBinary
         for agent in self.agents.values():
-            if isinstance(agent, GridAttackingAgent):
+            if isinstance(agent, AttackingAgent):
                 agent.action_space['attack'] = MultiBinary(1)
 
-    def process_attack(self, attacking_agent, **kwargs):
+    def process_attack(self, attacking_agent_id, attack, **kwargs):
         """
         Determine which agent the attacking agent successfully attacks and return
         that agent's id. If the attack fails, return None.
 
-        attacking_agent (agent):
-            The agent that has chosen to attack.
+        attacking_agent_id (str):
+            The agent that we are processing.
+
+        attack (bool):
+            True if the agent has chosen to attack, otherwise False.
         """
-        for agent in self.agents.values():
-            if agent.id == attacking_agent.id:
-                # Cannot attack yourself
-                continue
-            elif not agent.is_alive:
-                # Cannot attack dead agents
-                continue
-            elif attacking_agent.team == agent.team:
-                # Cannot attack agents on the same team
-                continue
-            elif abs(attacking_agent.position[0] - agent.position[0]) > attacking_agent.attack_range or \
-               abs(attacking_agent.position[1] - agent.position[1]) > attacking_agent.attack_range:
-                # Agent too far away
-                continue
-            else:
-                # Agent was successfully attacked
-                return agent.id
+        if attack:
+            attacking_agent = self.agents[attacking_agent_id]
+            for attacked_agent in self.agents.values():
+                if attacked_agent.id == attacking_agent_id:
+                    # Cannot attack yourself
+                    continue
+                elif not attacked_agent.is_alive:
+                    # Cannot attack dead agents
+                    continue
+                elif attacking_agent.team == attacked_agent.team:
+                    # Cannot attack agents on the same team
+                    continue
+                elif abs(attacking_agent.position[0] - attacked_agent.position[0]) > attacking_agent.attack_range or \
+                     abs(attacking_agent.position[1] - attacked_agent.position[1]) > attacking_agent.attack_range:
+                    # Agent too far away
+                    continue
+                else:
+                    self.life_state.modify_health(attacked_agent.id, -attacking_agent.attack_strength, **kwargs)
+                    self.life_state.modify_health(attacking_agent_id, attacking_agent.attack_strength, **kwargs)
