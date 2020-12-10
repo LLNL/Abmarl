54835c31ff0a1df19bedfd4b480df46d5f7e5597
Eddie Rusu <rusu1@llnl.gov>
2020-12-10 12:13:47 -0800

    Updated team components

--- admiral/envs/components/team.py
+++ admiral/envs/components/team.py
@@ -1,4 +1,6 @@
 
+import numpy as np
+
 from admiral.envs import Agent
 
 class TeamAgent(Agent):
@@ -17,3 +19,22 @@ class TeamAgent(Agent):
         Agent is configured if team is set.
         """
         return super().configured and self.team is not None
+
+class TeamComponent:
+    def __init__(self, agents=None, number_of_teams=None, **kwargs):
+        self.number_of_teams = number_of_teams
+        self.agents = agents
+
+class TeamObserver:
+    def __init__(self, team=None, agents=None, **kwargs):
+        self.team = team
+        self.agents = agents
+    
+        from gym.spaces import Box, Dict
+        for agent in agents.values():
+            agent.observation_space['team'] = Dict({
+                agent_id: Box(0, self.team.number_of_teams, (1,), np.int) for agent_id in agents
+            })
+    
+    def get_obs(self, *args, **kwargs):
+        return {other.id: self.agents[other.id].team for other in self.agents.values()}
