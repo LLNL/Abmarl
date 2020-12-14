800236c360149b12008a2100f057d79dff425838
Eddie Rusu <rusu1@llnl.gov>
2020-12-14 12:48:40 -0800

    Updated test health component

--- tests/test_health_component.py
+++ tests/test_health_component.py
@@ -1,6 +1,6 @@
 
-from admiral.envs.components.death_life import HealthAgent, LifeAgent
-from admiral.envs.components.death_life import DyingComponent
+from admiral.envs.components.health import HealthAgent, LifeAgent
+from admiral.envs.components.health import LifeState
 
 def test_health_agents():
     agents = {
@@ -21,7 +21,7 @@ def test_health_agents():
     assert agents['agent3'].min_health == 0.0
     assert agents['agent3'].max_health == 5.0
 
-def test_dying_component():
+def test_life_state():
     agents = {
         'agent0': LifeAgent(id='agent0', min_health=0.0, max_health=5.0, initial_health=3.4),
         'agent1': LifeAgent(id='agent1', min_health=0.0, max_health=5.0, initial_health=2.4),
@@ -44,30 +44,33 @@ def test_dying_component():
     assert agents['agent3'].max_health == 5.0
     assert agents['agent3'].is_alive == True
 
-    component = DyingComponent(agents=agents, entropy=0.5)
-    component.reset()
+    state = LifeState(agents=agents, entropy=0.5)
+    for agent in agents.values():
+        state.reset(agent)
+
     assert agents['agent0'].health == 3.4
     assert agents['agent1'].health == 2.4
     assert 0.0 <= agents['agent2'].health <= 5.0
     assert 0.0 <= agents['agent3'].health <= 5.0
 
-    component.apply_entropy(agents['agent0'])
-    component.apply_entropy(agents['agent1'])
+    state.apply_entropy(agents['agent0'])
+    state.apply_entropy(agents['agent1'])
     assert agents['agent0'].health == 2.9
     assert agents['agent1'].health == 1.9
 
     for _ in range(10):
-        component.apply_entropy(agents['agent0'])
-        component.apply_entropy(agents['agent1'])
-        component.apply_entropy(agents['agent2'])
-        component.apply_entropy(agents['agent3'])
+        state.apply_entropy(agents['agent0'])
+        state.apply_entropy(agents['agent1'])
+        state.apply_entropy(agents['agent2'])
+        state.apply_entropy(agents['agent3'])
 
     assert agents['agent0'].is_alive is False
     assert agents['agent1'].is_alive is False
     assert agents['agent2'].is_alive is False
     assert agents['agent3'].is_alive is False
 
-    component.reset()
+    for agent in agents.values():
+        state.reset(agent)
     assert agents['agent0'].min_health == 0.0
     assert agents['agent0'].max_health == 5.0
     assert agents['agent0'].initial_health == 3.4
