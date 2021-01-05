ec04e0fb976041f8cdcf46339ec36dfce621db7e
Eddie Rusu <rusu1@llnl.gov>
2021-01-05 09:33:59 -0800

    Added mask observer and position-restricted mask observer

--- admiral/envs/components/position_restricted_observers.py
+++ admiral/envs/components/position_restricted_observers.py
@@ -1,13 +1,27 @@
 
+from gym.spaces import Dict, MultiBinary
 
-from admiral.envs.components.observers import AgentObservingAgent # TODO: probably change this name...
+from admiral.envs import Agent
 from admiral.envs.components.position import PositionAgent, PositionObserver
 from admiral.envs.components.team import TeamObserver
 from admiral.envs.components.health import HealthObserver, LifeObserver
 
+class AgentObservingAgent(Agent): # TODO: probably change this name...
+    def __init__(self, agent_view=None, **kwargs):
+        """
+        Agents can see other agents up to some maximal distance away, indicated
+        by the view.
+        """
+        super().__init__(**kwargs)
+        self.agent_view = agent_view
+    
+    @property
+    def configured(self):
+        return super().configured and self.agent_view is not None
+
 def obs_filter(obs, agent, agents, null_value):
     if isinstance(agent, PositionAgent) and \
-        isinstance(agent, AgentObservingAgent):
+       isinstance(agent, AgentObservingAgent):
         for other in agents.values():
             if other.id == agent.id: continue # Don't modify yourself
             if not isinstance(other, PositionAgent): continue # Can't modify based on position
@@ -16,6 +30,33 @@ def obs_filter(obs, agent, agents, null_value):
             if r_diff > agent.agent_view or c_diff > agent.agent_view: # Agent too far away to observe
                 obs[other.id] = null_value
     return obs
+
+class MaskObserver:
+    def __init__(self, agents=agents, **kwargs):
+        self.agents = agents
+        for agent in agents.values():
+            if isinstance(agent, AgentObservingAgent):
+                agent.observation_space['mask'] = Dict({
+                    other: MultiBinary(1) for other in agents.values()
+                })
+    
+    def get_obs(self, agent, **kwargs):
+        if isinstance(agent, AgentObservingAgent):
+            return {'mask': {other: True for other in self.agents.values()}}
+        else:
+            return {}
+    
+    @property
+    def null_value(self):
+        return 0
+
+class PositionRestrictedMaskObserver(MaskObserver):
+    """
+    Set the mask value to False for any agent that is too far way from the observing
+    agent.
+    """
+    def get_obs(self, agent, **kwargs):
+        return obs_filter(super().get_obs(), agent, self.agents, self.null_value)
 
 class PositionRestrictedTeamObserver(TeamObserver):
     """
