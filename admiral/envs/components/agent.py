09096b88149fa9d40a751971ef46f48d99e095da
Eddie Rusu <rusu24edward@gmail.com>
2021-01-11 18:11:05 -0800

    SpeedAngleAgents can have initial speed, banking angle, and ground angle

--- admiral/envs/components/agent.py
+++ admiral/envs/components/agent.py
@@ -207,16 +207,30 @@ class SpeedAngleAgent(Agent):
 
     max_banking_angle_change (float):
         The maximum amount by which an agent can change its banking angle.
+
+    initial_speed (float):
+        The agent's initial speed.
+    
+    initial_banking_angle (float):
+        The agent's initial banking angle.
+    
+    initial_ground_angle (float):
+        The agent's initial ground angle.
     """
     def __init__(self, min_speed=None, max_speed=None, max_acceleration=None, \
-                 max_banking_angle=None, max_banking_angle_change=None, **kwargs):
+                 max_banking_angle=None, max_banking_angle_change=None, \
+                 initial_speed=None, initial_banking_angle=None, \
+                 initial_ground_angle=None, **kwargs):
         self.min_speed = min_speed
         self.max_speed = max_speed
         self.max_acceleration = max_acceleration
+        self.initial_speed = initial_speed
         self.speed = None # Should be set by the state handler
 
         self.max_banking_angle = max_banking_angle
         self.max_banking_angle_change = max_banking_angle_change
+        self.initial_banking_angle = initial_banking_angle
+        self.initial_ground_angle = initial_ground_angle
         self.banking_angle = None # Should be set by the state handler
     
     @property
