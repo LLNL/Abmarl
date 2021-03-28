dc00a2aeb8c48478ff98d4e696992dc3136c0c5a
Eddie Rusu <rusu24edward@gmail.com>
2021-03-27 20:11:48 -0700

    partial observability applies to many observers at once

--- admiral/envs/components/wrappers/observer_wrapper.py
+++ admiral/envs/components/wrappers/observer_wrapper.py
@@ -11,28 +11,53 @@ def obs_filter_step(distance, view):
     return 0 if distance > view else 1
 
 class PositionRestrictedObservationWrapper:
-    def __init__(self, observer, obs_filter=obs_filter_step, obs_norm=np.inf, agents=None, **kwargs):
-        self.observer = observer
+    def __init__(self, observers, obs_filter=obs_filter_step, obs_norm=np.inf, agents=None, **kwargs):
+        # TODO: Remove the mask observer and include masking here.
+        self.observers = observers
         self.obs_filter = obs_filter
         self.agents = agents
         self.obs_norm = obs_norm
-    
+
     def get_obs(self, agent, **kwargs):
-        obs = self.observer.get_obs(agent, **kwargs)
-        if not isinstance(agent, PositionAgent):
-            # Agent does not have a position, so we cannot filter based on position.
-            # We return the observation as is
-            return obs
-        else:
-            for other in self.agents.values():
-                if other.id == agent.id: continue
-                elif not isinstance(other, PositionAgent): continue # Cannot filter out agents who have no position
-                elif np.random.uniform() <= \
-                    self.obs_filter(np.linalg.norm(agent.position - other.position, self.obs_norm), agent.agent_view): \
-                        continue # We perfectly observed this agent
-                else:
-                    # We did not observe the agent, so we have to modify the obs
-                    for obs_type, obs_content in obs.items():
-                        if other.id in obs_content: # TODO: observer should see all agents for consistent input, so this if check will go away when we fix that
-                            obs_content[other.id] = self.observer.null_value
-        return obs
+        all_obs = {}
+
+        other_filter = []
+        for other in self.agents.values():
+            if other.id == agent.id: continue
+            elif not isinstance(other, PositionAgent): continue # Cannot filter out agents who have no position
+            elif np.random.uniform() <= \
+                self.obs_filter(np.linalg.norm(agent.position - other.position, self.obs_norm), agent.agent_view): \
+                    continue # We perfectly observed this agent
+            else:
+                other_filter.append(other.id)
+
+        for observer in self.observers:
+            obs = observer.get_obs(agent, **kwargs)
+            for obs_type, obs_content in obs.items():
+                for other in other_filter:
+                    if other in obs_content: # TODO: observers should see all agents for consistent input, so this if check will go away when we fix that
+                        obs_content[other] = observer.null_value
+
+            all_obs.update(obs)
+        
+        return all_obs
+    
+    # def get_obs(self, agent, **kwargs):
+    #     obs = {**observer.get_obs(agent, **kwargs) for observer in self.observers}
+    #     if not isinstance(agent, PositionAgent):
+    #         # Agent does not have a position, so we cannot filter based on position.
+    #         # We return the observation as is
+    #         return obs
+    #     else:
+    #         for other in self.agents.values():
+    #             if other.id == agent.id: continue
+    #             elif not isinstance(other, PositionAgent): continue # Cannot filter out agents who have no position
+    #             elif np.random.uniform() <= \
+    #                 self.obs_filter(np.linalg.norm(agent.position - other.position, self.obs_norm), agent.agent_view): \
+    #                     continue # We perfectly observed this agent
+    #             else:
+    #                 # We did not observe the agent, so we have to modify the obs
+    #                 for obs_type, obs_content in obs.items():
+    #                     if other.id in obs_content: # TODO: observers should see all agents for consistent input, so this if check will go away when we fix that
+    #                         obs_content[other.id] = self.observers.null_value
+    #     return obs
