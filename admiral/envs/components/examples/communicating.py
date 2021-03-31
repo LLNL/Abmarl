Fixed bug with agent obs fusing when no broadcast was sent

admiral/envs/components/examples/communicating.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
         'agent1': CommunicatingAgent(id='agent1', initial_position=np.array([3, 3]), team=0, broadcast_range=4, agent_view=3),
         'agent2': CommunicatingAgent(id='agent2', initial_position=np.array([5, 0]), team=1, broadcast_range=4, agent_view=2),
         'agent3': CommunicatingAgent(id='agent3', initial_position=np.array([6, 9]), team=1, broadcast_range=4, agent_view=2),
-        'agent4': CommunicatingAgent(id='agent4', initial_position=np.array([4, 6]), team=1, broadcast_range=4, agent_view=3),
+        'agent4': CommunicatingAgent(id='agent4', initial_position=np.array([4, 7]), team=1, broadcast_range=4, agent_view=3),
     }
     env = BroadcastCommunicationEnv(
         region=10,
@@ -106,6 +106,7 @@ if __name__ == "__main__":
     env.render(fig=fig)
 
     action_dict = {agent.id: agent.action_space.sample() for agent in env.agents.values()}
+    # action_dict = {agent.id: {'broadcast': 0} for agent in env.agents.values()}
     print('\nActions:')
     print(action_dict)
     env.step(action_dict)

admiral/envs/components/wrappers/observer_wrapper.py
@@ -139,8 +139,7 @@ class TeamBasedCommunicationWrapper:
             # If I'm not on the same team, then I will not see its observation,
             #   but I will still see its own attributes.
             for broadcasting_agent in self.agents.values():
-                if isinstance(broadcasting_agent, PositionAgent) and isinstance(receiving_agent, PositionAgent):
-                    # TODO: AND the broadcasting agent IS broadcasting
+                if isinstance(broadcasting_agent, PositionAgent) and isinstance(receiving_agent, PositionAgent) and broadcasting_agent.broadcasting:
                     distance = np.linalg.norm(broadcasting_agent.position - receiving_agent.position, self.obs_norm)
                     if distance > broadcasting_agent.broadcast_range: continue # Too far from this broadcasting agent
                     elif isinstance(receiving_agent, TeamAgent) and isinstance(broadcasting_agent, TeamAgent) and receiving_agent.team == broadcasting_agent.team: