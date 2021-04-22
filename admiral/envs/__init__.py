73b60052751c0134cb926139d6033b687cfeb318
Eddie Rusu <rusu24edward@gmail.com>
2021-04-22 15:48:49 -0700

    Removed simple agent from __init__ and added generic Agent

--- admiral/envs/__init__.py
+++ admiral/envs/__init__.py
@@ -1,10 +1,10 @@
 
-from .agent_based_simulation import AgentBasedSimulation
-from admiral.envs.components.agent import Agent, ObservingAgent, ActingAgent
+from .agent_based_simulation import AgentBasedSimulation, Agent
+# from admiral.envs.components.agent import Agent, ObservingAgent, ActingAgent
 
-class SimpleAgent(ObservingAgent, ActingAgent):
-    """
-    A SimpleAgent that observes and acts. For use with environments that don't
-    use components.
-    """
-    pass
+# class SimpleAgent(ObservingAgent, ActingAgent):
+#     """
+#     A SimpleAgent that observes and acts. For use with environments that don't
+#     use components.
+#     """
+#     pass
