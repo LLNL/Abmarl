6483c731074c3d674bdafcacc123aea363c7788c
Eddie Rusu <rusu24edward@gmail.com>
2021-04-22 16:34:41 -0700

    Skeleton for four agent component tests

---
+++ tests/envs/simulation_components/test_agent.py
@@ -0,0 +1,16 @@
+
+import pytest
+
+from admiral.envs.components import ComponentAgent
+
+def test_component_agent_initial_position():
+    pass
+
+def test_component_agent_min_max_health():
+    pass
+
+def test_component_agent_initial_health():
+    pass
+
+def test_component_agent_team():
+    pass
