3bb51c81ac27b0433959ec2d72119ea9bd8388c6
Eddie Rusu <rusu1@llnl.gov>
2021-05-24 11:35:33 -0700

    Removed flatten wrapper

--- admiral/envs/wrappers/__init__.py
+++ admiral/envs/wrappers/__init__.py
@@ -3,7 +3,4 @@ from .wrapper import Wrapper
 
 # SAR-based wrappers
 from .sar_wrapper import SARWrapper
-from .flatten_wrapper import FlattenActionWrapper, FlattenWrapper
 from .ravel_discrete_wrapper import RavelDiscreteWrapper
-
-from .communication_wrapper import CommunicationHandshakeWrapper
