dedcaceac05c479e3e68abd740ccb155dc0d06ce
Eddie Rusu <rusu1@llnl.gov>
2021-06-07 11:18:01 -0700

    Updated design anaylsis docs with more detailed analysis script

--- examples/analysis_prototype.py
+++ examples/analysis_prototype.py
@@ -2,17 +2,24 @@ def run(sim, trainer):
     """
     Analyze the behavior of your trained policies using the simulation and trainer
     from your RL experiment.
+
+    Args:
+        sim:
+            Simulation Manager object from the experiment.
+        trainer:
+            Trainer that computes actions using the trained policies.
     """
     # Run the simulation with actions chosen from the trained policies
     policy_agent_mapping = trainer.config['multiagent']['policy_mapping_fn']
-    # for episode in range(num_episodes):
     for episode in range(100):
         print('Episode: {}'.format(episode))
         obs = sim.reset()
+        done = {agent: False for agent in obs}
         while True: # Run until the episode ends
             # Get actions from policies
             joint_action = {}
             for agent_id, agent_obs in obs.items():
+                if done[agent_id]: continue # Don't get actions for done agents
                 policy_id = policy_agent_mapping(agent_id)
                 action = trainer.compute_action(agent_obs, policy_id=policy_id)
                 joint_action[agent_id] = action
