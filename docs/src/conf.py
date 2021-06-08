0c74f8d7d323c853f5cd99af884a0707a7839ae4
Eddie Rusu <rusu1@llnl.gov>
2021-06-08 10:33:58 -0700

    Updated the version number for pypi consistency

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -13,7 +13,7 @@ copyright = '2021, LLNL'
 author = 'Edward Rusu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
