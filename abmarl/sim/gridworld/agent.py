123704c2ac522847528159c4949d5f2be407a404
Eddie Rusu <rusu24edward@gmail.com>
2021-09-24 13:54:16 -0700

    Health agents are randomly assigned health if they are not given one

--- abmarl/sim/gridworld/agent.py
+++ abmarl/sim/gridworld/agent.py
@@ -189,9 +189,12 @@ class HealthAgent(GridWorldAgent):
     
     @initial_health.setter
     def initial_health(self, value):
-        assert type(value) in [int, float], "Initial health must be a numeric value."
-        assert 0 < value <= 1, "Initial value must be between 0 and 1."
-        self._initial_health = value
+        if value is None:
+            self._initial_health = np.random.uniform(0, 1)
+        else:
+            assert type(value) in [int, float], "Initial health must be a numeric value."
+            assert 0 < value <= 1, "Initial value must be between 0 and 1."
+            self._initial_health = value
 
     @property
     def active(self):
