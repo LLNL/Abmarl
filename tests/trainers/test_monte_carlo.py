0ba1732019ea8f018adad84ac82214eefaa0cb82
Eddie Rusu <rusu1@llnl.gov>
2022-02-14 13:53:16 -0800

    Added epsilon-soft policy test

--- tests/trainers/test_monte_carlo.py
+++ tests/trainers/test_monte_carlo.py
@@ -16,45 +16,34 @@ def test_exploring_starts_corridor():
         observation_space=ref_agent.observation_space
     )
     trainer = OnPolicyMonteCarloTrainer(sim=sim, policy=policy)
-    trainer.train(iterations=1000, horizon=20)
+    trainer.train(iterations=10_000, horizon=20)
 
     obs = sim.reset()
-    for _ in range(10):
+    for _ in range(20):
         actions = trainer.compute_actions(obs)
         obs, reward, done, info = sim.step(actions)
         if done['__all__']:
             break
     assert done['__all__']
 
-    # sim, q_table, policy = trainer.train(iterations=1)
 
-    # assert isinstance(sim.sim, AllStepManager)
-    # assert isinstance(sim.sim.sim, RavelDiscreteWrapper)
-    # assert isinstance(sim.sim.sim.sim, MultiCorridor)
+def test_epsilon_soft():
+    sim = AllStepManager(RavelDiscreteWrapper(MultiCorridor(num_agents=1)))
+    ref_agent = sim.agents['agent0']
+    policy = EpsilonSoftPolicy(
+        action_space=ref_agent.action_space,
+        observation_space=ref_agent.observation_space
+    )
+    trainer = OnPolicyMonteCarloTrainer(sim=sim, policy=policy)
+    trainer.train(iterations=1000, horizon=20)
 
-    # assert q_table.shape == (sim.observation_space.n, sim.action_space.n)
-    # assert isinstance(policy, RandomFirstActionPolicy)
-test_exploring_starts_corridor()
-
-# def test_epsilon_soft():
-#     sim = AllStepManager(RavelDiscreteWrapper(MultiCorridor(num_agents=1)))
-#     sim, q_table, policy = epsilon_soft(sim, iteration=1000, horizon=20)
-
-#     assert isinstance(sim, GymWrapper)
-#     assert isinstance(sim.sim, AllStepManager)
-#     assert isinstance(sim.sim.sim, RavelDiscreteWrapper)
-#     assert isinstance(sim.sim.sim.sim, MultiCorridor)
-
-#     assert q_table.shape == (sim.observation_space.n, sim.action_space.n)
-#     assert isinstance(policy, EpsilonSoftPolicy)
-
-#     obs = sim.reset()
-#     for _ in range(10):
-#         action = policy.act(obs)
-#         obs, reward, done, info = sim.step(action)
-#         if done:
-#             break
-#     assert done
+    obs = sim.reset()
+    for _ in range(20):
+        actions = trainer.compute_actions(obs)
+        obs, reward, done, info = sim.step(actions)
+        if done['__all__']:
+            break
+    assert done['__all__']
 
 
 # def test_off_policy():
