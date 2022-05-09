318dd4467aa221b2c455dafa097a64e43edd3c0e
Eddie Rusu <rusu1@llnl.gov>
2022-05-09 13:45:44 -0700

    Ray 1.9.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     install_requires=[
         'tensorflow',
         'gym<0.22',
-        'ray[rllib]==1.8.0',
+        'ray[rllib]==1.9.0',
         'matplotlib',
         'seaborn',
     ],
