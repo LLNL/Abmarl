72e980efefe05e0e741d87b43c62da5d97d2a2de
Eddie Rusu <rusu1@llnl.gov>
2022-06-02 13:47:17 -0700

    Dynamic manager test for only manager stuff

--- tests/test_dynamic_order_manager.py
+++ tests/test_dynamic_order_manager.py
@@ -3,52 +3,48 @@ from gym.spaces import Discrete
 from matplotlib.pyplot import isinteractive
 import pytest
 
-from abmarl.sim import Agent, DynamicOrderSimulation, AgentBasedSimulation
+from abmarl.sim import PrincipleAgent, DynamicOrderSimulation, AgentBasedSimulation
 from abmarl.managers import DynamicOrderManager
 
 
-    class SequentiallyFinishingSim(DynamicOrderSimulation):
-        def __init__(self, **kwargs):
-            self.agents = {f'agent{i}': PrincipleAgent(id=f'agent{i}') for i in range(4)}
+class SequentiallyFinishingSim(DynamicOrderSimulation):
+    def __init__(self, **kwargs):
+        self.agents = {f'agent{i}': PrincipleAgent(id=f'agent{i}') for i in range(4)}
 
-        def reset(self, **kwargs):
-            self.next_agent = 'agent3'
-            self.done_agents = set()
+    def reset(self, **kwargs):
+        self.next_agent = 'agent3'
+        self.done_agents = set()
 
-        def step(self, action_dict, **kwargs):
-            next_agents = set()
-            for agent_id, action in action_dict.items():
-                self.done_agents.add(agent_id)
-                for next_ids in action:
-                    next_agents.add(next_ids)
-            self.next_agent = next_agents
+    def step(self, action_dict, **kwargs):
+        next_agents = set()
+        for agent_id, action in action_dict.items():
+            self.done_agents.add(agent_id)
+            for next_ids in action:
+                next_agents.add(next_ids)
+        self.next_agent = next_agents
 
-        def render(self, **kwargs):
-            pass
+    def render(self, **kwargs):
+        pass
 
-        def get_obs(self, agent_id, **kwargs):
-            return agent_id
+    def get_obs(self, agent_id, **kwargs):
+        return agent_id
 
-        def get_reward(self, agent_id, **kwargs):
-            return {}
+    def get_reward(self, agent_id, **kwargs):
+        return {}
 
-        def get_done(self, agent_id, **kwargs):
-            return agent_id in self.done_agents
+    def get_done(self, agent_id, **kwargs):
+        return agent_id in self.done_agents
 
-        def get_all_done(self, **kwargs):
-            return all([self.get_done(agent_id) for agent_id in self.agents])
+    def get_all_done(self, **kwargs):
+        return all([self.get_done(agent_id) for agent_id in self.agents])
 
-        def get_info(self, agent_id, **kwargs):
-            return {}
+    def get_info(self, agent_id, **kwargs):
+        return {}
 
 
 class ABS(AgentBasedSimulation):
     def __init__(self, **kwargs):
-        self.agents = {
-            f'agent{i}': Agent(
-                f'agent{i}', observation_space=Discrete(3), action_space=Discrete(4)
-            ) for i in range(4)
-        }
+        self.agents = {f'agent{i}': PrincipleAgent(f'agent{i}') for i in range(4)}
 
     def reset(self, **kwargs):
         pass
@@ -94,23 +90,23 @@ def test_dynamic_order_manager_stepping():
     )
     sim.reset()
     action = {
-        'agent3': [0, 3]
+        'agent3': ['agent0', 'agent3']
     }
     obs, _, done, _ = sim.step(action)
     assert obs == {'agent0': 'agent0', 'agent3': 'agent3'}
     assert done == {'agent0': False, 'agent3': True, '__all__': False}
 
-    action = {'agent0': [1]}
+    action = {'agent0': ['agent1']}
     obs, _, done, _ = sim.step(action)
     assert obs == {'agent1': 'agent1'}
     assert done == {'agent1': False, '__all__': False}
 
-    action = {'agent1': [0, 2]}
+    action = {'agent1': ['agent0', 'agent2']}
     obs, _, done, _ = sim.step(action)
     assert obs == {'agent0': 'agent0', 'agent2': 'agent2'}
     assert done == {'agent0': True, 'agent2': False, '__all__': False}
 
-    action = {'agent2': [1, 2]}
+    action = {'agent2': ['agent1', 'agent2']}
     obs, _, done, _ = sim.step(action)
     assert obs == {'agent1': 'agent1', 'agent2': 'agent2'}
     assert done == {'agent1': True, 'agent2': True, '__all__': True}
@@ -122,15 +118,15 @@ def test_dynamic_order_manager_action_from_already_done_agents():
     )
     sim.reset()
     action = {
-        'agent3': [0, 3]
+        'agent3': ['agent0', 'agent3']
     }
     obs, _, done, _ = sim.step(action)
     assert obs == {'agent0': 'agent0', 'agent3': 'agent3'}
     assert done == {'agent0': False, 'agent3': True, '__all__': False}
 
     action = {
-        'agent3': [3],
-        'agent0': [1],
+        'agent3': ['agent3'],
+        'agent0': ['agent1'],
     }
     with pytest.raises(AssertionError):
         sim.step(action)
