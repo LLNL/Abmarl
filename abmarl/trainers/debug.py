b20f0c87883f1564d376514bf366842c56e83a6e
Eddie Rusu <rusu1@llnl.gov>
2022-06-28 11:37:40 -0700

    Debug trainer made with an output directory

--- abmarl/trainers/debug.py
+++ abmarl/trainers/debug.py
@@ -7,7 +7,29 @@ from abmarl.trainers.base import MultiPolicyTrainer
 class DebugTrainer(MultiPolicyTrainer):
     """
     Debug the training setup.
+
+    The DebugTrainer generates episodes using the simulation and policies. Rather
+    than training those policies, The DebugTrainer simply dumps the observations,
+    actions, rewards, and dones to disk.
     """
+    def __init__(self, output_dir=None, **kwargs):
+        super().__init__(**kwargs)
+        self.output_dir = output_dir
+
+    @property
+    def output_dir(self):
+        """
+        The directory for where to dump the episode data.
+        """
+        return self._output_dir
+
+    @output_dir.setter
+    def output_dir(self, value):
+        assert type(value) is str, "Output directory must be a string."
+        if not os.path.exists(value):
+            os.makedirs(value)
+        self._output_dir = value
+
     def train(self, iterations=5, render=False, **kwargs):
         """
         Generate episodes of data.
@@ -25,7 +47,7 @@ class DebugTrainer(MultiPolicyTrainer):
             observations, actions, rewards, dones = self.generate_episode(render=render, **kwargs)
 
             # Setup dump files
-            with open(os.path.join(output_dir, f"Episode_{i}.txt"), 'w') as debug_dump:
+            with open(os.path.join(self.output_dir, f"Episode_{i}.txt"), 'w') as debug_dump:
                 debug_dump.write("Observations:\n")
                 pprint(observations, stream=debug_dump)
                 debug_dump.write("\nActions:\n")
