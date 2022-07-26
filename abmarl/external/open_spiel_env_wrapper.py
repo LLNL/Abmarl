e08c41177f85b36c445c8d482c2eae28cf588b41
Eddie Rusu <rusu1@llnl.gov>
2022-07-26 12:27:25 -0700

    Assuming spaces are discrete

--- abmarl/external/open_spiel_env_wrapper.py
+++ abmarl/external/open_spiel_env_wrapper.py
@@ -1,4 +1,5 @@
 
+from gym.spaces import Discrete
 from open_spiel.python.rl_environment import TimeStep, StepType
 
 from abmarl.sim.agent_based_simulation import Agent
@@ -7,12 +8,18 @@ from abmarl.managers import TurnBasedManager, SimulationManager
 class OpenSpielWrapper:
     def __init__(self, sim, **kwargs):
         assert isinstance(sim, SimulationManager)
+        # The wrapper assumes that each space is discrete, so we should check for
+        # that.
+        for agent in self.sim.agents.values():
+            assert isinstance(agent.observation_space, Discrete) and \
+                isinstance(agent.action_space, Discrete), \
+                "OpenSpielWrapper can only work with simulations that have all Discrete spaces."
         self.sim = sim
         self._should_reset = True
 
     @property
     def num_players(self):
-        pass
+        return sum([1 for agent in self.sim.agents.values() if isinstance(agent, Agent)])
 
     @property
     def is_turn_based(self):
@@ -49,12 +56,6 @@ class OpenSpielWrapper:
         step_type = StepType.LAST if done else StepType.MID
         self._should_reset = step_type == StepType.LAST
 
-        # observations = {
-        #     "info_state": obs,
-        #     "legal_actions": self.get_legal_actions(),
-        #     "current_player": 0, # TODO: figure out the logic for this!
-        # }
-
         observations = {
             "info_state": obs,
             "legal_actions": {
@@ -74,8 +75,8 @@ class OpenSpielWrapper:
     def observation_spec(self):
         return {
             agent.id: {
-                'info_state': agent.observation_space,
-                'legal_actions': num_actions,
+                'info_state': (agent.observation_space.n,),
+                'legal_actions': (agent.action_space.n,),
                 'current_player': ()
             } for agent in self.sim.agents.values() if isinstance(agent, Agent)
         }
@@ -83,13 +84,12 @@ class OpenSpielWrapper:
     def action_spec(self):
         return {
             agent.id: {
-                'num_actions': agent.action_space,
-                'min': min,
-                'max': max,
+                'num_actions': agent.action_space.n,
+                'min': 0,
+                'max': agent.action_space.n - 1,
                 'dtype': int
             } for agent in self.sim.agents.values() if isinstance(agent, Agent)
         }
-        # TODO: I might need to make a function that converts gym space to open-spiel spec.
 
     def get_legal_actions(self, agent_id):
         """
@@ -99,18 +99,4 @@ class OpenSpielWrapper:
         in each time step. This function can be overwritten in a derived class
         to add logic for obtaining the actual legal actions available.
         """
-        return [self.sim.agents[agent_id].action_space.n] # TODO: Something like this.
-
-    # def get_legal_actions(self):
-    #     """
-    #     Return the legal actions available to the player.
-
-    #     By default, this wrapper uses all the available actions as the legal actions
-    #     in each time step. This function can be overwritten in a derived class
-    #     to add logic for obtaining the actual legal actions available.
-    #     """
-    #     return {
-    #         agent.id: [agent.action_space.n] # TODO: Something like this
-    #         for agent in self.sim.agents.values() if isinstance(agent, Agent)
-    #     }
-
+        return [self.sim.agents[agent_id].action_space.n]
