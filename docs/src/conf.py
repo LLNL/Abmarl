5c56ffc9c159255d6aa3b612232563ad27baa224
Eddie Rusu <rusu1@llnl.gov>
2022-08-04 15:19:34 -0700

    Version 0.2.4

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -13,7 +13,7 @@ copyright = '2021, LLNL'
 author = 'Edward Rusu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------
