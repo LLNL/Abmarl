ac332d6c8eef5d8c61440fc468485d8461cfa106
Eddie Rusu <rusu1@llnl.gov>
2022-09-14 11:23:33 -0700

    Number of attacks parameter to attacking agents

--- abmarl/sim/gridworld/agent.py
+++ abmarl/sim/gridworld/agent.py
@@ -201,11 +201,13 @@ class AttackingAgent(ActingAgent, GridWorldAgent):
     """
     Agents that can attack other agents.
     """
-    def __init__(self, attack_range=None, attack_strength=None, attack_accuracy=None, **kwargs):
+    def __init__(self, attack_range=None, attack_strength=None, attack_accuracy=None, 
+                 number_of_attacks=None, **kwargs):
         super().__init__(**kwargs)
         self.attack_range = attack_range
         self.attack_strength = attack_strength
         self.attack_accuracy = attack_accuracy
+        self.number_of_attacks = number_of_attacks
 
     @property
     def attack_range(self):
@@ -250,6 +252,27 @@ class AttackingAgent(ActingAgent, GridWorldAgent):
         self._attack_accuracy = value
 
     @property
+    def number_of_attacks(self):
+        """
+        The number of attacks the agent can make per turn.
+
+        This parameter is interpreted differently by each attack actor, but generally
+        it specifies how many attacks this agent will carry out in a single step.
+        See specific attack documentation for more information.
+        """
+        # TODO: The question of one vs all in the attack actors can be answered
+        # with this parameter. The agent can attack up to this many other agents
+        # per turn. Should update some actors to work with this parameter.
+        return self._number_of_attacks
+
+    @number_of_attacks.setter
+    def number_of_attacks(self, value):
+        assert type(value) is int, "Number of attacks must be an integer."
+        assert value >= 0, "Number of attacks must be nonnegative."
+        self._number_of_attacks = value
+
+    @property
     def configured(self):
         return super().configured and self.attack_range is not None and \
-            self.attack_strength is not None and self.attack_accuracy is not None
+            self.attack_strength is not None and self.attack_accuracy is not None and \
+            self.number_of_attacks is not None
