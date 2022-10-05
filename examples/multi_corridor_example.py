9e564b0b277c7e69bc37f6a8e8ce6b572915d8ec
Ephraim Rusu <rusu1@llnl.gov>
2022-10-05 10:56:31 -0700

    Arbitarily random policies in multicorridor

--- examples/multi_corridor_example.py
+++ examples/multi_corridor_example.py
@@ -1,6 +1,7 @@
 from abmarl.examples import MultiCorridor
 from abmarl.managers import TurnBasedManager
 from abmarl.external import MultiAgentWrapper
+from ray.rllib.examples.policy.random_policy import RandomPolicy
 
 sim = MultiAgentWrapper(TurnBasedManager(MultiCorridor()))
 
@@ -10,12 +11,14 @@ register_env(sim_name, lambda sim_config: sim)
 
 ref_agent = sim.sim.agents['agent0']
 policies = {
-    'corridor': (None, ref_agent.observation_space, ref_agent.action_space, {})
+    'corridor_0': (None, ref_agent.observation_space, ref_agent.action_space, {}),
+    'corridor_1': (RandomPolicy, ref_agent.observation_space, ref_agent.action_space, {}),
+    # 'corridor_1': (None, ref_agent.observation_space, ref_agent.action_space, {}),
 }
 
 
 def policy_mapping_fn(agent_id):
-    return 'corridor'
+    return f'corridor_{int(agent_id[-1]) % 2}'
 
 
 # Experiment parameters
@@ -46,7 +49,7 @@ params = {
             # "lr": 0.0001,
             # --- Parallelism ---
             # Number of workers per experiment: int
-            "num_workers": 7,
+            "num_workers": 0,
             # Number of simulations that each worker starts: int
             "num_envs_per_worker": 1, # This must be 1 because we are not "threadsafe"
         },
