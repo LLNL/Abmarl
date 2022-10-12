ab4656588659785af5dee7580e7e03b9de965ce2
Ephraim Rusu <rusu1@llnl.gov>
2022-10-12 12:47:00 -0700

    Debug script utilizes debug trainer for output directory logic

--- abmarl/debug.py
+++ abmarl/debug.py
@@ -9,24 +9,15 @@ def run(full_config_path, parameters):
     experiment_mod = adu.custom_import_module(full_config_path)
     title = "DEBUG_" + experiment_mod.params['experiment']['title']
 
-    # Copy the configuration module to the output directory
-    import os
-    import shutil
-    import time
-    base = experiment_mod.params['ray_tune'].get('local_dir', os.path.expanduser("~"))
-    output_dir = os.path.join(
-        base, 'abmarl_results/{}_{}'.format(
-            title, time.strftime('%Y-%m-%d_%H-%M')
-        )
-    )
-    experiment_mod.params['ray_tune']['local_dir'] = output_dir
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    shutil.copy(full_config_path, output_dir)
-
     # Debug the simulation
     sim = experiment_mod.params['experiment']['sim_creator'](
         experiment_mod.params['ray_tune']['config']['env_config']
     )
-    trainer = DebugTrainer(sim=sim.sim, output_dir=output_dir)
+    trainer = DebugTrainer(
+        sim=sim.sim,
+        name=title,
+        output_dir=experiment_mod.params['ray_tune'].get('local_dir')
+    )
+    import shutil
+    shutil.copy(full_config_path, trainer.output_dir)
     trainer.train(iterations=parameters.episodes, render=parameters.render)
