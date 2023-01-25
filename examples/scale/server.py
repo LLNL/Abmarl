52f4fcc48f5faf39e14634bd59acada8e79c4149
Ephraim Rusu <rusu1@llnl.gov>
2023-01-25 11:31:41 -0800

    Filled in some details in the server script

--- examples/scale/server.py
+++ examples/scale/server.py
@@ -7,7 +7,7 @@ from ray.rllib.env.policy_server_input import PolicyServerInput
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--ip-head',
+    '--server_address',
     type=str,
     default='localhost',
     help='The ip address of the remote server.'
@@ -26,58 +26,83 @@ parser.add_argument(
     "socket for incoming SAR data.",
 )
 parser.add_argument(
-    "--checkpoint",
+    "--restore",
     type=str,
     default='',
-    help="Continue training from a previous run. Checkpoiint should be the full "
+    help="Continue training from a previous run. Restore should be the full "
     "path to the output directory file according to Abmarl's structure."
 )
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    # server's address
-    server_address = args.ip_head
-    server_port = 9900
-    print(f'server {args.ip_head}:{server_port}')
+    def _input(ioctx):
+        # We are remote worker or we are local worker with num_workers=0:
+        # Create a PolicyServerInput.
+        if ioctx.worker_index > 0 or ioctx.worker.num_workers == 0:
+            return PolicyServerInput(
+                ioctx,
+                args.server_address,
+                args.base_port + ioctx.worker_index - (1 if ioctx.worker_index > 0 else 0),
+            )
+        # No InputReader (PolicyServerInput) needed.
+        else:
+            return None
 
     # Policies
-    from abmarl.sim.corridor import MultiCorridor
-    agents = MultiCorridor().agents
-    # NOTE: This ^ is a temporary work around until rllib can get the spaces
+    # TODO: Get the policies from the configuration file instead
+    # NOTE: This is a temporary work around until rllib can get the spaces
     # from the client.
+    from abmarl.examples.sim.multi_corridor import MultiCorridor
+    agents = MultiCorridor().agents
     policies = {
-        agent.id: (None, agent.observation_space, agent.action_space, {})
-        for agent in agents.values()
+        'target': (None, agents['target'].observation_space, agents['target'].action_space, {}),
+        'runner': (None, agents['runner0'].observation_space, agents['runner0'].action_space, {}),
     }
-    policy_mapping_fn = lambda agent_id: agent_id
 
-    ray.init()
+    def policy_mapping_fn(agent_id):
+        return 'runner' if agent_id.startswith('runner') else 'target'
 
-    # configure the trainer
+
+    # Trainer config.
+    # Get the config from the configuration file
     config = {
-        # Use the connector server to generate experiences.
-        "input": (
-            lambda ioctx: PolicyServerInput(ioctx, server_address, server_port)
-        ),
-        # Give the observation and action space directly
+        # Use the `PolicyServerInput` to generate experiences.
+        "input": _input,
+        # Disable OPE, since the rollouts are coming from online clients.
+        "off_policy_estimation_methods": {},
+        # Indicate that the Trainer we setup here doesn't need an actual env.
+        # Allow spaces to be determined by user (see below).
         "env": None,
+
+        # The following should come from the configuration file
         "multiagent": {
             "policies": policies,
             "policy_mapping_fn": policy_mapping_fn,
         },
-        # Use a single worker process to run the server.
-        "num_workers": 0,
-        # Disable OPE, since the rollouts are coming from online clients.
-        "input_evaluation": [],
+        # checkpoint direcory
+        "output": "/usr/workspace/rusu1/decision_superiority/maze_results_sparse_reward_5/",
+        # Use n worker processes to listen on different ports.
+        "num_workers": args.num_workers,
+        # Set to INFO so we'll see the server's actual address:port.
+        "log_level": "INFO",
+        "train_batch_size": 50_000,
+        "rollout_fragment_length": 500,
     }
+
+    # Attempt to restore from checkpoint, if possible.
+    if args.restore and os.path.exists(args.restore):
+        restore_from_path = open(args.restore).read()
+        # TODO: We need to dig into the directory to get the checkpoint from which to restore
+    else:
+        restore_from_path = None
 
     # Run with Tune for auto env and trainer creation and TensorBoard.
     tune.run(
-        "A2C",
+        "A2C", # TODO: Get from configuration file
         config=config,
-        stop={
+        stop={ # TODO: Get from configuration file
             'episodes_total': 2000,
         },
-        verbose=2
+        restore=restore_from_path
     )
