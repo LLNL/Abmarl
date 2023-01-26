c1c0942e942d01f3cfe5971c3fc9fa14d8fd46a0
Ephraim Rusu <rusu1@llnl.gov>
2023-01-26 14:31:33 -0800

    cleaned up the client-server shell script

--- examples/scale/client_server.sh
+++ examples/scale/client_server.sh
@@ -1,60 +1,65 @@
 #!/bin/bash
-#SBATCH --job-name=multi-corridor-train
-#SBATCH --nodes=2
+#SBATCH --job-name=abmarl-scale-training
+#SBATCH --nodes=<nodes>
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:30:00
-#SBATCH --partition=pvis
+#SBATCH --time=<time>
+#SBATCH --partition=<partition>
 #SBATCH --exclusive
 #SBATCH --no-kill
 #SBATCH --output="slurm-%j.out"
 #SBATCH --ip-isolate yes
 
-# Run with sbatch client_server.sh
+# Run command: sbatch client_server.sh
 
 # Source the virtual environment
+source <virtual_env>
 source /usr/WS1/rusu1/decision_superiority/v_pybind/bin/activate
 
 # Getting the node names
 nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
 nodes_array=($nodes)
 
-head_node=${nodes_array[0]}
-head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+server_node=${nodes_array[0]}
+server_node_ip=$(srun --nodes=1 --ntasks=1 -w "$server_node" hostname --ip-address)
 
-# if we detect a space character in the head node IP, we'll
+# if we detect a space character in the server node IP, we'll
 # convert it to an ipv4 address. This step is optional.
-if [[ "$head_node_ip" == *" "* ]]; then
-IFS=' ' read -ra ADDR <<<"$head_node_ip"
+if [[ "$server_node_ip" == *" "* ]]; then
+IFS=' ' read -ra ADDR <<<"$server_node_ip"
 if [[ ${#ADDR[0]} -gt 16 ]]; then
-  head_node_ip=${ADDR[1]}
+  server_node_ip=${ADDR[1]}
 else
-  head_node_ip=${ADDR[0]}
+  server_node_ip=${ADDR[0]}
 fi
-echo "IPV6 address detected. We split the IPV4 address as $head_node_ip"
+echo "IPV6 address detected. We split the IPV4 address as $server_node_ip"
 fi
 
-port=6379
-ip_head=$head_node_ip:$port
-export ip_head # TODO: Is this needed?
-echo "IP Head: $ip_head"
+# TODO: I don't think I need these lines
+# port=6379
+# ip_head=$server_node_ip:$port
+# export ip_head # TODO: Is this needed?
+# echo "Ray dashboard configured to run on: $ip_head"
 
-echo "Starting HEAD at $head_node"
-srun --nodes=1 --ntasks=1 -w "$head_node" --output="slurm-%j-HEAD.out" \
-  python3 -u ./server.py --ip-head $head_node_ip &
+echo "Starting server at $server_node"
+srun --nodes=1 --ntasks=1 -w "$server_node" --output="slurm-%j-SERVER.out" \
+  python3 -u ./server.py --server-address $server_node_ip --base-port <base_port> \
+  --num-workers <num_workers> &
 
-# Give the computer time to launch the server node before launching the clients.
-sleep 180
+# TODO: Do we still need this?
+# # Give the computer time to launch the server node before launching the clients.
+# sleep 180
 
 # number of nodes other than the head node
 echo "SLURM JOB NUM NODES " $SLURM_JOB_NUM_NODES
-worker_num=$((SLURM_JOB_NUM_NODES - 1))
+clients_num=$((SLURM_JOB_NUM_NODES - 1))
 
-for ((i = 1; i <= worker_num; i++)); do
+for ((i = 1; i <= clients_num; i++)); do
     node_i=${nodes_array[$i]}
-    echo "Starting WORKER $i at $node_i"
+    echo "Starting client $i at $node_i"
     srun --nodes=1 --ntasks=1 -w "$node_i" --output="slurm-%j-$node_i.out" \
-      python3 -u ./client.py --ip-head $head_node_ip &
+      python3 -u ./client.py --server-address $server_node_ip --port <base_port+node_i> \
+      --inference-mode <inference_mode> &
     sleep 5
 done
 
