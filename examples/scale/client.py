8a2db5ac3c77e5c94cd5243e93a4cb29f5a4df89
Ephraim Rusu <rusu1@llnl.gov>
2023-01-26 14:09:45 -0800

    Cleaned up the client file

--- examples/scale/client.py
+++ examples/scale/client.py
@@ -3,47 +3,51 @@ import argparse
 
 from ray.rllib.env.policy_client import PolicyClient
 
+from abmarl.tools import utils as adu
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--inference-mode", type=str, default="local", choices=["local", "remote"])
-parser.add_argument(
-    "--ip-head",
+    '--server_address',
     type=str,
     default='localhost',
-    help="The ip address of the remote server."
+    help='The ip address of the remote server.'
 )
+parser.add_argument(
+    '--port',
+    type=int,
+    default=9900,
+    help='The client should connect to this server port.'
+)
+parser.add_argument(
+    "--inference-mode", type=str, default="local", choices=["local", "remote"])
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    # server's address
-    address = args.ip_head
-    port = 9900
-    ip_head = 'http://' + address + ":" + str(port)
-
-    # simulation environment
-    from abmarl.sim.corridor import MultiCorridor
-    from abmarl.managers import TurnBasedManager
-    from abmarl.external import MultiAgentWrapper
-    env = MultiAgentWrapper(TurnBasedManager(MultiCorridor()))
+    # Get the simulation from the configuration file
+    experiment_mod = adu.custom_import_module('/Users/rusu1/Abmarl/examples/reach_the_target_example.py')
+    sim = experiment_mod.sim
 
     # policy client
-    client = PolicyClient(ip_head, inference_mode=args.inference_mode)
+    client = PolicyClient(
+        f"http://{args.server_address}:{args.port}",
+        inference_mode=args.inference_mode
+    )
     
     # Start data generation
-    obs = env.reset()
-    eid = client.start_episode(training_enabled=True)
-    done = {agent: False for agent in obs}
-    while True:
-        action_t = client.get_action(eid, obs)
-        action_dict = {
-            agent_id: action
-            for agent_id, action in action_t.items() if not done[agent_id]
-        }
-        obs, reward, done, info = env.step(action_dict)
-        client.log_returns(eid, reward, info=info)
-        if done['__all__']:
-            client.end_episode(eid, obs)
-            obs = env.reset()
-            done = {agent: False for agent in obs}
-            eid = client.start_episode(training_enabled=True)
+    for i in range(experiment_mod.params['ray_tune']['stop']['episodes_total']):
+        eid = client.start_episode(training_enabled=True)
+        obs = sim.reset()
+        done = {agent: False for agent in obs}
+        for j in range(experiment_mod.params['ray_tune']['config']['horizon']):
+            action_t = client.get_action(eid, obs)
+            action_dict = {
+                agent_id: action
+                for agent_id, action in action_t.items() if not done[agent_id]
+            }
+            obs, reward, done, info = sim.step(action_dict)
+            client.log_returns(eid, reward, multiagent_done_dict=done, info=info)
+            if done['__all__']:
+                break
+        client.end_episode(eid, obs)
