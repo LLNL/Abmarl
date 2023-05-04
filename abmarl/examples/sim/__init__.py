dfcb4d86065f93fdf0603996f5dea0c3f994fafb
Ephraim Rusu <rusu1@llnl.gov>
2023-05-04 13:23:42 -0700

    Added multi agent grid sim to examples for testing

--- abmarl/examples/sim/__init__.py
+++ abmarl/examples/sim/__init__.py
@@ -3,4 +3,5 @@ from .team_battle_example import BattleAgent, TeamBattleSim
 from .maze_navigation import MazeNavigationAgent, MazeNaviationSim
 from .multi_agent_sim import EmptyABS, MultiAgentSim, MultiAgentGymSpacesSim, \
     MultiAgentContinuousGymSpaceSim, MultiAgentSameSpacesSim
+from .multi_agent_grid_sim import MultiAgentGridSim
 from .reach_the_target import ReachTheTargetSim, RunningAgent, TargetAgent, BarrierAgent
