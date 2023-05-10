3b63cc68c3f021a907584cfc75f3bb518b4cc069
Ephraim Rusu <rusu1@llnl.gov>
2023-05-10 12:48:01 -0700

    Test state for small grids

--- tests/sim/gridworld/test_state.py
+++ tests/sim/gridworld/test_state.py
@@ -4,6 +4,7 @@ import numpy as np
 from abmarl.sim.gridworld.grid import Grid
 from abmarl.sim.gridworld.state import PositionState, HealthState, StateBaseComponent
 from abmarl.sim.gridworld.agent import HealthAgent, GridWorldAgent
+import pytest
 
 
 def test_position_state():
@@ -24,6 +25,60 @@ def test_position_state():
     assert grid[0, 1] == {'agent0': agents['agent0']}
     assert grid[1, 2] == {'agent1': agents['agent1']}
     assert grid[2, 0] == {'agent2': agents['agent2']}
+
+
+def test_position_state_small_grid():
+    grid = Grid(1, 2, overlapping={1: {1, 2}, 2: {1, 2}, 3: {3}})
+    agents = {
+        'agent0': GridWorldAgent(id='agent0', encoding=1, initial_position=np.array([0, 0])),
+        'agent1': GridWorldAgent(id='agent1', encoding=2, initial_position=np.array([0, 0])),
+        'agent2': GridWorldAgent(id='agent2', encoding=3),
+        'agent3': GridWorldAgent(id='agent3', encoding=3),
+        'agent4': GridWorldAgent(id='agent4', encoding=2),
+        'agent5': GridWorldAgent(id='agent5', encoding=1)
+    }
+    # Encoding 3 can only go on (0, 1) because (0, 0) is taken and can't be overlapped.
+    # If agents 2 and 3 were placed after 4 and 5, they would likely not have a
+    # cell, so we see that the order of the agents matters in their initial placement.
+    position_state = PositionState(grid=grid, agents=agents)
+    position_state.reset()
+    assert 'agent0' in grid[0, 0]
+    assert 'agent1' in grid[0, 0]
+    assert 'agent2' in grid[0, 1]
+    assert 'agent3' in grid[0, 1]
+    assert 'agent4' in grid[0, 0]
+    assert 'agent5' in grid[0, 0]
+
+
+    # This will fail because agents 0 and 1 have taken all available cells, so there
+    # is no where to put encoding 3
+    agents = {
+        'agent0': GridWorldAgent(id='agent0', encoding=1, initial_position=np.array([0, 0])),
+        'agent1': GridWorldAgent(id='agent1', encoding=2, initial_position=np.array([0, 1])),
+        'agent2': GridWorldAgent(id='agent2', encoding=3)
+    }
+    position_state = PositionState(grid=grid, agents=agents)
+    with pytest.raises(RuntimeError):
+        position_state.reset()
+
+
+    # This may fail because agent 1 might have taken the last avilable cell.
+    # We have set two seeds: one where it passes and another where it fails
+    np.random.seed(24)
+    agents = {
+        'agent0': GridWorldAgent(id='agent0', encoding=1, initial_position=np.array([0, 0])),
+        'agent1': GridWorldAgent(id='agent1', encoding=2),
+        'agent2': GridWorldAgent(id='agent2', encoding=3),
+    }
+    position_state = PositionState(grid=grid, agents=agents)
+    position_state.reset()
+    assert 'agent0' in grid[0, 0]
+    assert 'agent1' in grid[0, 0]
+    assert 'agent2' in grid[0, 1]
+
+    np.random.seed(17)
+    with pytest.raises(RuntimeError):
+        position_state.reset()
 
 
 def test_health_state():
