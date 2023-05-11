504e67deb093da4fbc7d41f9ba571359645ce496
Ephraim Rusu <rusu1@llnl.gov>
2023-05-11 10:27:24 -0700

    Pulled apart position state into sub-components

--- abmarl/sim/gridworld/state.py
+++ abmarl/sim/gridworld/state.py
@@ -23,6 +23,27 @@ class PositionState(StateBaseComponent):
     """
     Manage the agents' positions in the grid.
     """
+    @property
+    def ravelled_positions_available(self):
+        """
+        A dictionary mapping the enodings to a set of positions available to
+        agents of that encoding at reset. The set should contain cells represented
+        in their ravelled form.
+        """
+        return self._ravelled_positions_available
+
+    @ravelled_positions_available.setter
+    def ravelled_positions_available(self, value):
+        assert type(value) is dict, "Ravelled Positions available must be a dictionary."
+        for encoding, ravelled_positions_available in value.items():
+            assert type(encoding) is int, "Ravelled Position keys must be integers."
+            assert type(ravelled_positions_available) is set, \
+                "Ravelled Position values must be sets."
+            for ravelled_cell in ravelled_positions_available:
+                assert type(ravelled_cell) is int, "Available cells must be integers. " \
+                    "They should be the ravelled presentation of the cell."
+        self._ravelled_positions_available = value
+
     def reset(self, **kwargs):
         """
         Give agents their starting positions.
@@ -32,12 +53,14 @@ class PositionState(StateBaseComponent):
         """
         max_encoding = max([agent.encoding for agent in self.agents.values()])
         ravelled_available_positions = \
-            self.ravelled_positions_available_at_reset(max_encoding=max_encoding)
+            self._build_available_positions(max_encoding=max_encoding)
         self.grid.reset()
         # First place agents with initial positions.
         for agent in self.agents.values():
             if agent.initial_position is not None:
                 r, c = agent.initial_position
+                # TODO: Assert that the agent can be placed in this position according
+                # to ravelled positions available
                 assert self.grid.place(agent, (r, c)), "All initial positions must " + \
                     "be unique or agents with the same initial positions must be overlappable."
                 for encoding in range(1, max_encoding + 1):
@@ -74,21 +97,64 @@ class PositionState(StateBaseComponent):
                             # been removed from this encoding
                             continue
 
-    def ravelled_positions_available_at_reset(self, max_encoding=0, **kwargs):
+    def _build_available_positions(self, max_encoding=0, **kwargs):
         """
         Define the positions that are available per encoding.
 
         Args:
             max_encoding: The maximum number of encodings in the simulation.
-        Returns:
-            A dictionary mapping the enodings to a set of positions available to
-                agents of that encoding at reset. The set should be the cell represented
-                in its ravelled form.
         """
-        return {
+        self.ravelled_positions_available = {
             encoding: set(i for i in range(self.rows * self.cols))
             for encoding in range(1, max_encoding + 1)
         }
+    
+    def _update_available_positions(self, agent_just_placed, **kwargs):
+        """
+        Update the available positions based on the agent that was just placed.
+        """
+        for encoding, positions_available in self.ravelled_positions_available.items():
+            # Remove this cell from any encoding where overlapping is False
+            if encoding not in self.grid.overlapping.get(agent_just_placed.encoding, {}):
+                try:
+                    positions_available.remove(
+                        np.ravel_multi_index(agent_just_placed.position, (self.rows, self.cols))
+                    )
+                except KeyError:
+                    # Catch a key error because this cell might have already
+                    # been removed from this encoding
+                    continue
+
+    def _place_initial_position_agent(self, ip_agent_to_place, **kwargs):
+        """
+        Place an agent with an initial position.
+        """
+        ravelled_initial_position = np.ravel_multi_index(
+            ip_agent_to_place.initial_position,
+            (self.rows, self.cols)
+        )
+        assert ravelled_initial_position in \
+            self.ravelled_positions_available[ip_agent_to_place.encoding], \
+            f"Cell {ip_agent_to_place.initial_position} is not available for " \
+            f"{ip_agent_to_place.id}."
+        assert self.grid.place(ip_agent_to_place, ip_agent_to_place.initial_position)
+        self._update_available_positions(ip_agent_to_place)
+
+    def _place_variable_position_agent(self, var_agent_to_place, **kwargs):
+        """
+        Place an agent with a variable position.
+
+        This implementation randomly places the agent in one of its available cells.
+        """
+        try:
+            ravelled_position = np.random.choice(
+                [*self.ravelled_positions_available[var_agent_to_place.encoding]], 1)
+        except ValueError:
+            raise RuntimeError(f"Could not find a cell for {var_agent_to_place.id}") from None
+        else:
+            r, c = np.unravel_index(ravelled_position.item(), shape=(self.rows, self.cols))
+            assert self.grid.place(var_agent_to_place, (r, c))
+            self._update_available_positions(var_agent_to_place)
 
 
 class HealthState(StateBaseComponent):
