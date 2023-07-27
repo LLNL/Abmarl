85a4944221ef8a32667a7954792bce58a91db7e3
Ephraim Rusu <rusu1@llnl.gov>
2023-07-27 14:02:04 -0700

    GW component registry by type and name with support to register custom components.

---
+++ abmarl/sim/gridworld/registry.py
@@ -0,0 +1,63 @@
+from .base import GridWorldBaseComponent
+from .actor import ActorBaseComponent, MoveActor, CrossMoveActor, BinaryAttackActor, \
+    EncodingBasedAttackActor, RestrictedSelectiveAttackActor, SelectiveAttackActor
+from .done import DoneBaseComponent, ActiveDone, TargetAgentDone, OneTeamRemainingDone
+from .observer import ObserverBaseComponent, AbsoluteGridObserver, SingleGridObserver, \
+    MultiGridObserver, AbsolutePositionObserver
+from .state import StateBaseComponent, PositionState, TargetBarriersFreePlacementState, \
+    MazePlacementState, HealthState
+
+# TODO: Figure out how to wrap
+from.wrapper import ComponentWrapper, ActorWrapper, ObserverWrapper, RavelActionWrapper, \
+    ExclusiveChannelActionWrapper
+
+_subclass_check_mapping = {
+    'actor': ActorBaseComponent,
+    'done': DoneBaseComponent,
+    'observer': ObserverBaseComponent,
+    'state': StateBaseComponent
+}
+
+_registered_components = {
+    'actor': {
+        MoveActor,
+        CrossMoveActor,
+        BinaryAttackActor,
+        EncodingBasedAttackActor,
+        RestrictedSelectiveAttackActor,
+        SelectiveAttackActor
+    }, 'done': {
+        ActiveDone,
+        TargetAgentDone,
+        OneTeamRemainingDone
+    }, 'observer': {
+        AbsoluteGridObserver,
+        SingleGridObserver,
+        MultiGridObserver,
+        AbsolutePositionObserver
+    }, 'state': {
+        PositionState,
+        TargetBarriersFreePlacementState,
+        MazePlacementState,
+        HealthState
+    }
+}
+
+_registry = {
+    component_type: {component.__name__: component for component in components}
+    for component_type, components in _registered_components.items()
+}
+
+def register(component):
+    registered = False
+    for component_type, base_component in _subclass_check_mapping.items():
+        if issubclass(component, base_component):
+            _registered_components[component_type].add(component)
+            _registry[component_type][component.__name__] = component
+            registered = True
+            break # Assumes that a component is a subclass of only one base component
+
+    if not registered:
+        raise TypeError(
+            f"{component.__name__} must be an actor, done, state, or observer component."
+        )
