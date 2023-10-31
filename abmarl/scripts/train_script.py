b6bd38ff6880016112dd124f2533c1a7b34cd2f3
Ephraim Rusu <rusu1@llnl.gov>
2023-10-31 11:53:29 -0700

    Reconfigrued the train cli command for better scripting support

--- abmarl/scripts/train_script.py
+++ abmarl/scripts/train_script.py
@@ -13,5 +13,19 @@ def create_parser(subparsers):
 
 
 def run(full_config_path):
-    from abmarl import train
-    train.run(full_config_path)
+    from abmarl.tools import utils as adu
+    from abmarl.train import _set_output_directory, _train_rllib
+    # Load the experiment as a module
+    experiment_mod = adu.custom_import_module(full_config_path)
+    params = experiment_mod.params
+
+    # Copy the configuration file
+    output_dir = _set_output_directory(params)
+    import os
+    import shutil
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    shutil.copy(full_config_path, output_dir)
+
+    # Train the policy
+    _train_rllib(params)
--- abmarl/train.py
+++ abmarl/train.py
@@ -1,31 +1,39 @@
-from abmarl.tools import utils as adu
 
-
-def run(full_config_path):
-    """Train MARL policies using the config_file."""
-
-    # Load the experiment as a module
-    experiment_mod = adu.custom_import_module(full_config_path)
-    title = experiment_mod.params['experiment']['title']
-
-    # Copy the configuration module to the output directory
+def _set_output_directory(params):
+    """
+    Set the output directory in the parameters.
+    """
     import os
-    import shutil
     import time
-    base = experiment_mod.params['ray_tune'].get('local_dir', os.path.expanduser("~"))
+    title = params['experiment']['title']
+    base = params['ray_tune'].get('local_dir', os.path.expanduser("~"))
     output_dir = os.path.join(
         base, 'abmarl_results/{}_{}'.format(
             title, time.strftime('%Y-%m-%d_%H-%M')
         )
     )
-    experiment_mod.params['ray_tune']['local_dir'] = output_dir
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    shutil.copy(full_config_path, output_dir)
+    params['ray_tune']['local_dir'] = output_dir
+    return output_dir
 
-    # Train with ray
+
+def _train_rllib(params):
+    """
+    Train MARL policies with RLlib using parameters dictionary.
+    """
     import ray
     from ray import tune
     ray.init()
-    tune.run(**experiment_mod.params['ray_tune'])
+    tune.run(**params['ray_tune'])
     ray.shutdown()
+
+
+def train(params):
+    """
+    Train MARL policies with RLlib using parameters dictionary.
+
+    Args:
+        Parameter dictionary that holds all the parameters for training.
+    """
+    # Copy the configuration module to the output directory
+    output_dir = _set_output_directory(params)
+    _train_rllib(params)
