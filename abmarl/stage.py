1b4cd0b8339c8ecfb3fbfbc03116b9c0e38502fc
Ephraim Rusu <rusu1@llnl.gov>
2023-10-31 12:29:18 -0700

    Unravelled the start and finish commands in stage

--- abmarl/stage.py
+++ abmarl/stage.py
@@ -13,8 +13,9 @@ from abmarl.tools import utils as adu
 from abmarl.managers import SimulationManager
 
 
-def _start(full_trained_directory, requested_checkpoint, seed=None):
-    """The elements that are common to both analyze and visualize."""
+
+def run_analysis(full_trained_directory, full_subscript, parameters):
+    """Analyze MARL policies from a saved policy through an analysis script"""
     # Load the experiment as a module
     # First, we must find the .py file in the directory
     py_files = [file for file in os.listdir(full_trained_directory) if file.endswith('.py')]
@@ -24,10 +25,10 @@ def _start(full_trained_directory, requested_checkpoint, seed=None):
     # Modify the number of workers in the configuration
     experiment_mod.params['ray_tune']['config']['num_workers'] = 1
     experiment_mod.params['ray_tune']['config']['num_envs_per_worker'] = 1
-    experiment_mod.params['ray_tune']['config']['seed'] = seed
+    experiment_mod.params['ray_tune']['config']['seed'] = parameters.seed
 
     checkpoint_dir, checkpoint_value = adu.checkpoint_from_trained_directory(
-        full_trained_directory, requested_checkpoint
+        full_trained_directory, parameters.requested_checkpoint
     )
     print(checkpoint_dir)
 
@@ -47,18 +48,6 @@ def _start(full_trained_directory, requested_checkpoint, seed=None):
         experiment_mod.params['ray_tune']['config']['env_config']
     )
 
-    return sim, trainer
-
-
-def _finish():
-    """Finish off the evaluation run."""
-    ray.shutdown()
-
-
-def run_analysis(full_trained_directory, full_subscript, parameters):
-    """Analyze MARL policies from a saved policy through an analysis script"""
-    sim, trainer = _start(full_trained_directory, parameters.checkpoint, parameters.seed)
-
     # The sim may be wrapped by an external wrapper, which we support, but we need
     # to unwrap it.
     if not isinstance(sim, SimulationManager):
@@ -68,7 +57,10 @@ def run_analysis(full_trained_directory, full_subscript, parameters):
     analysis_mod = adu.custom_import_module(full_subscript)
     analysis_mod.run(sim, trainer)
 
-    _finish()
+    ray.shutdown()
+
+
+              
 
 
 def run_visualize(full_trained_directory, parameters):
@@ -81,7 +73,39 @@ def run_visualize(full_trained_directory, parameters):
         except ImportError:
             matplotlib.use('TkAgg')
 
-    sim, trainer = _start(full_trained_directory, parameters.checkpoint, seed=parameters.seed)
+    # sim, trainer = _start(full_trained_directory, parameters.checkpoint, seed=parameters.seed)
+        """The elements that are common to both analyze and visualize."""
+    # Load the experiment as a module
+    # First, we must find the .py file in the directory
+    py_files = [file for file in os.listdir(full_trained_directory) if file.endswith('.py')]
+    assert len(py_files) == 1
+    full_path_to_config = os.path.join(full_trained_directory, py_files[0])
+    experiment_mod = adu.custom_import_module(full_path_to_config)
+    # Modify the number of workers in the configuration
+    experiment_mod.params['ray_tune']['config']['num_workers'] = 1
+    experiment_mod.params['ray_tune']['config']['num_envs_per_worker'] = 1
+    experiment_mod.params['ray_tune']['config']['seed'] = parameters.seed
+
+    checkpoint_dir, checkpoint_value = adu.checkpoint_from_trained_directory(
+        full_trained_directory, parameters.requested_checkpoint
+    )
+    print(checkpoint_dir)
+
+    # Setup ray
+    ray.init()
+
+    # Get the trainer
+    alg = get_trainable_cls(experiment_mod.params['ray_tune']['run_or_experiment'])
+    trainer = alg(
+        env=experiment_mod.params['ray_tune']['config']['env'],
+        config=experiment_mod.params['ray_tune']['config']
+    )
+    trainer.restore(os.path.join(checkpoint_dir, 'checkpoint-' + str(checkpoint_value)))
+
+    # Get the simulation
+    sim = experiment_mod.params['experiment']['sim_creator'](
+        experiment_mod.params['ray_tune']['config']['env_config']
+    )
 
     # Determine if we are single- or multi-agent case.
     def _multi_get_action(obs, done=None, sim=None, policy_agent_mapping=None, **kwargs):
@@ -159,4 +183,4 @@ def run_visualize(full_trained_directory, parameters):
             plt.pause(1)
         plt.close(fig)
 
-    _finish()
+    ray.shutdown()
