053c078eee3a8f8b0fbe4e93249e071735733e30
Ephraim Rusu <rusu1@llnl.gov>
2023-11-27 14:18:50 -0800

    Attempting to add external package if rllib is requested dep

--- setup.py
+++ setup.py
@@ -1,7 +1,44 @@
 import setuptools
+import subprocess
+
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
+
+def custom_find_packages():
+    """
+    Build external integration modules if those dependencies exist.
+    """
+    packages=[
+        'abmarl',
+        'abmarl.algs',
+        'abmarl.examples',
+        'abmarl.examples.sim',
+        'abmarl.managers',
+        'abmarl.policies',
+        'abmarl.scripts',
+        'abmarl.sim',
+        'abmarl.sim.gridworld',
+        'abmarl.sim.wrappers',
+        'abmarl.tools',
+        'abmarl.trainers',
+    ]
+    # TODO: This doesn't work because this function runs before dependencies are installed.
+    # Need to see if there is way to build dependencies frist and then run this function....
+    x = subprocess.Popen("pip3 list", shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+    if 'ray' in x:
+        packages.append('abmarl.external')
+        print("\n\n\n\n\nRllib is installed\n\n\n\n\n")
+    print("\n\n\n\n\nNot installed\n\n\n\n\n")
+
+
+    # x = subprocess.Popen("pip3 list", shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+    # if 'ray' in x:
+    #     print("\n\n\nRllib is installed!")
+    # if 'open-spiel' in x:
+    #     print("\n\n\nOpen Spiel installed!")
+
+    return packages
 
 setuptools.setup(
     name='abmarl',
@@ -29,19 +66,7 @@ setuptools.setup(
         'Featured Usage': 'https://abmarl.readthedocs.io/en/latest/featured_usage.html',
         'Documentation': 'https://abmarl.readthedocs.io/en/latest/index.html',
     },
-    packages=[
-        'abmarl',
-        'abmarl.algs',
-        'abmarl.examples',
-        'abmarl.external',
-        'abmarl.managers',
-        'abmarl.policies',
-        'abmarl.scripts',
-        'abmarl.sim',
-        'abmarl.tools',
-        'abmarl.trainers',
-        'abmarl.examples',
-    ],
+    packages=custom_find_packages(),
     install_requires=[
         'importlib-metadata<5.0',
         'numpy<1.24',
