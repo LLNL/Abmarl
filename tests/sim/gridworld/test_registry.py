9ba586644d50b0ef1aff9c053b36027d6c14dc43
Ephraim Rusu <rusu1@llnl.gov>
2024-03-19 14:58:43 -0700

    Added missing components to registry test

--- tests/sim/gridworld/test_registry.py
+++ tests/sim/gridworld/test_registry.py
@@ -3,14 +3,15 @@ import pytest
 
 from abmarl.sim.gridworld.actor import MoveActor, CrossMoveActor, \
     BinaryAttackActor, EncodingBasedAttackActor, RestrictedSelectiveAttackActor, \
-    SelectiveAttackActor
+    SelectiveAttackActor, DriftMoveActor
 from abmarl.sim.gridworld.done import ActiveDone, TargetAgentDone, \
-    OneTeamRemainingDone, TargetEncodingInactiveDone
+    OneTeamRemainingDone, TargetEncodingInactiveDone, TargetDestroyedDone
 from abmarl.sim.gridworld.observer import AbsoluteEncodingObserver, \
     PositionCenteredEncodingObserver, StackedPositionCenteredEncodingObserver, \
-    AbsolutePositionObserver
+    AbsolutePositionObserver, AmmoObserver
 from abmarl.sim.gridworld.state import PositionState, \
-    TargetBarriersFreePlacementState, MazePlacementState, HealthState
+    TargetBarriersFreePlacementState, MazePlacementState, HealthState, AmmoState, \
+    OrientationState
 from abmarl.sim.gridworld.registry import registry, register
 
 from abmarl.examples.sim.comms_blocking import BroadcastingActor, BroadcastingState, \
@@ -24,9 +25,11 @@ def test_built_in_registry():
     assert EncodingBasedAttackActor in registry['actor'].values()
     assert RestrictedSelectiveAttackActor in registry['actor'].values()
     assert SelectiveAttackActor in registry['actor'].values()
+    assert DriftMoveActor in registry['actor'].values()
 
     assert ActiveDone in registry['done'].values()
     assert TargetAgentDone in registry['done'].values()
+    assert TargetDestroyedDone in registry['done'].values()
     assert OneTeamRemainingDone in registry['done'].values()
     assert TargetEncodingInactiveDone in registry['done'].values()
 
@@ -34,11 +37,14 @@ def test_built_in_registry():
     assert PositionCenteredEncodingObserver in registry['observer'].values()
     assert StackedPositionCenteredEncodingObserver in registry['observer'].values()
     assert AbsolutePositionObserver in registry['observer'].values()
+    assert AmmoObserver in registry['observer'].values()
 
     assert PositionState in registry['state'].values()
     assert TargetBarriersFreePlacementState in registry['state'].values()
     assert MazePlacementState in registry['state'].values()
     assert HealthState in registry['state'].values()
+    assert AmmoState in registry['state'].values()
+    assert OrientationState in registry['state'].values()
 
 
 def test_custom_registrations():
